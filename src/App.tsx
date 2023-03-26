80a85374d8c288710f4578fc849ebcff16e9d897
leemyeongjae <leemyeongjae@leemyeongjaeui-MacBookPro.local>
2023-03-26 21:31:01 +0900

    CHORE :: App.tsx에 테스트했던 코드 없앰📝

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import Tooltip from "components/common/Tooltip";
 import React from "react";
 
 const App = () => {
-  return (
-    <div>
-      <Tooltip message="이" position="top-left">
-        <div>
-          <div style={{ fontSize: 20 }}>검색</div>
-        </div>
-      </Tooltip>
-      <div>asDF</div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
