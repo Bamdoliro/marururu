28afc643a0dab5fc47528452399dec00cdf0b322
leemyeongjae <leemyeongjae@leemyeongjaeui-MacBookPro.local>
2023-03-26 22:22:09 +0900

    FEAT :: 미니 툴팁 만듦✨

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import MiniTooltip from "components/common/MiniTooltip";
 import React from "react";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <MiniTooltip message="텍스트" position="top-right">
+        <div style={{ fontSize: 20 }}>검색</div>
+      </MiniTooltip>
+    </div>
+  );
 };
 
 export default App;
