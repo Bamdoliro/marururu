c0f40cc12e43ca23ea868ff176b46253e3958bd5
leemyeongjae <leemyeongjae@leemyeongjaeui-MacBookPro.local>
2023-03-30 15:51:53 +0900

    CHORE :: App.tsx에 Tooltip 컴포넌트 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import MiniTooltip from "components/common/MiniTooltip";
-import Tooltip from "components/common/Tooltip";
 import React from "react";
 
 const App = () => {
-  return (
-    <div>
-      <MiniTooltip message="이것은 상자입니다. 히히 😁😁😁😁😁" left>
-        <div>📦</div>
-      </MiniTooltip>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
