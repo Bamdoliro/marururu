07aa1e401003011f795fb74796d49a121ef653ca
SEOKKAMONI <sj01048260279@gmail.com>
2023-04-04 15:30:42 +0900

    ADD :: 리뷰 반영

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -1,19 +1,21 @@
-import { ButtonPropsType } from 'types/common/button.type';
-import * as S from './style';
+import { ButtonHTMLAttributes } from "react";
+import { ButtonOptionType } from "types/common/button.type";
+import * as S from "./style";
 
-const Button = ({
-     onClick, 
-     value, 
-     option, 
-     width, 
-     icon 
-    }: ButtonPropsType) => {
-    return (
-        <S.Button onClick={ onClick } option={ option } style={{ width }}>
-            {icon && <S.ButtonIcon src={ icon } />}
-            <S.ButtonText>{value}</S.ButtonText>
-        </S.Button>
-    )
+interface PropsType extends ButtonHTMLAttributes<HTMLButtonElement> {
+  value: string;
+  option: ButtonOptionType;
+  width?: string;
+  icon?: string;
 }
+
+const Button = ({ onClick, value, option, width, icon }: PropsType) => {
+  return (
+    <S.Button onClick={onClick} option={option} style={{ width }}>
+      {icon && <S.ButtonIcon src={icon} />}
+      <S.ButtonText>{value}</S.ButtonText>
+    </S.Button>
+  );
+};
 
 export default Button;
--- src/types/common/button.type.ts
+++ src/types/common/button.type.ts
@@ -1,10 +1,6 @@
-import { ButtonHTMLAttributes } from "react";
-
-export type ButtonOptionType = "PRIMARY" | "SECONDARY" | "TERTIARY" | "QUATERNARY" | "DELETE";
-
-export interface ButtonPropsType extends ButtonHTMLAttributes<HTMLButtonElement>  {
-    value: string;
-    option: ButtonOptionType;
-    width?: string;
-    icon?: string;
-}
+export type ButtonOptionType =
+  | "PRIMARY"
+  | "SECONDARY"
+  | "TERTIARY"
+  | "QUATERNARY"
+  | "DELETE";
