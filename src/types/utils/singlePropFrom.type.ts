247cd387899264c1211a11e19fde5f46159e1754
leemyeongjae <leemyeongjae@leemyeongjaeui-MacBookPro.local>
2023-04-18 08:35:33 +0900

    REFACTOR :: tooltip.type.ts 파일 수정

--- src/types/utils/singlePropFrom.type.ts
+++
@@ -1,9 +0,0 @@
-export type SinglePropFrom<O extends object, K extends keyof O> = [
-  K
-] extends infer KUT
-  ? K extends any
-    ? KUT extends [infer KU extends keyof O]
-      ? Pick<O, K> & Partial<Record<Exclude<KU, K>, never>>
-      : never
-    : never
-  : never;
