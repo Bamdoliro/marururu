68afbc53070cd081a9cfd35847e99ce7b0833cf4
leemyeongjae <leemyeongjae@leemyeongjaeui-MacBookPro.local>
2023-04-20 20:14:32 +0900

    FIX :: style.ts에서 안쓰는 요소 삭제

--- src/components/common/LabelInput/style.ts
+++ src/components/common/LabelInput/style.ts
@@ -14,26 +14,6 @@ export const Desc = styled.p`
   grid-column: 1 / 3;
 `;
 
-export const Input = styled.input`
-  ${font.p2}
-  color: ${color.gray800};
-
-  height: 48px;
-  padding: 10px 16px;
-  background-color: ${color.white};
-  border: 1px solid ${color.gray400};
-  border-radius: 6px;
-
-  &::placeholder {
-    color: ${color.gray500};
-  }
-
-  &:focus {
-    border: 1px solid ${color.maruDefault};
-    outline: 2px solid rgba(20, 112, 255, 0.25);
-  }
-`;
-
 export const Button = styled.button`
   ${font.btn1};
   color: ${color.white};
