e64b648da6e669aa1ef40e78a7fbba0319d087bd
SEOKKAMONI <sj01048260279@gmail.com>
2023-05-12 11:28:31 +0900

    ADD :: Styles

---
+++ src/styles/color.ts
@@ -0,0 +1,22 @@
+export const color = {
+  white: "#ffffff",
+  orange: "#FF763B",
+  red: "#F44336",
+
+  maruDefault: "#1470FF",
+  maruHigh: "#3081FF",
+  maruRow: "#0C57CB",
+
+  gray50: "#FCFCFD",
+  gray100: "#F8F8F9",
+  gray200: "#EEF0F2",
+  gray300: "#DDDEE3",
+  gray400: "#BBBCC2",
+  gray500: "#9497A0",
+  gray600: "#70737C",
+  gray700: "#595B61",
+  gray750: "#414550",
+  gray800: "#373B44",
+  gray850: "#2B2E36",
+  gray900: "#21242C",
+};
