e0c2b51a698f645022cff0302c17dd476830fa75
leemyeongjae <leemyeongjae@leemyeongjaeui-MacBookPro.local>
2023-05-21 23:30:52 +0900

    FIX :: 테스트 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,9 @@
 "use client";
 
-import DropdownMenu from "@/components/common/DropdownMenu/dropdownMenu";
-import Column from "@/components/common/Flex/column";
 import AppLayout from "@/layouts/AppLayout";
 
 const Home = () => {
-  return (
-    <AppLayout>
-      <Column gap="0px">
-        <DropdownMenu
-          desc="옵션"
-          dropdownMenuData={[
-            { dropdownItemText: "옵션1" },
-            { dropdownItemText: "옵션2" },
-            { dropdownItemText: "옵션3" },
-          ]}
-        />
-        <DropdownMenu
-          desc="옵션"
-          dropdownMenuData={[
-            { dropdownItemText: "옵션1" },
-            { dropdownItemText: "옵션2" },
-            { dropdownItemText: "옵션3" },
-          ]}
-        />
-        <DropdownMenu
-          desc="옵션"
-          dropdownMenuData={[
-            { dropdownItemText: "옵션1" },
-            { dropdownItemText: "옵션2" },
-            { dropdownItemText: "옵션3" },
-          ]}
-        />
-        <DropdownMenu
-          desc="옵션"
-          dropdownMenuData={[
-            { dropdownItemText: "옵션1" },
-            { dropdownItemText: "옵션2" },
-            { dropdownItemText: "옵션3" },
-          ]}
-        />
-      </Column>
-    </AppLayout>
-  );
+  return <AppLayout>home이다.</AppLayout>;
 };
 
 export default Home;
