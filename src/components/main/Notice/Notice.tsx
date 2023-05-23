cb6094422f62af05ad2f114ffcbe9ec443725ec7
SEOKKAMONI <sj01048260279@gmail.com>
2023-05-23 15:55:32 +0900

    ADD :: Link

--- src/components/main/Notice/Notice.tsx
+++ src/components/main/Notice/Notice.tsx
@@ -4,6 +4,7 @@ import RightArrowIcon from "@/components/common/Icon/RightArrow";
 import NoticeItem from "./NoticeItem";
 import styled from "styled-components";
 import { useRouter } from "next/navigation";
+import Link from "@/components/common/Link/link";
 import { MainNoticeItemData } from "@/data/main";
 
 const Notice = () => {
@@ -11,10 +12,10 @@ const Notice = () => {
 
   return (
     <StyledNotice>
-      <NoticeButton onClick={() => router.push("/notice")}>
+      <Link onClick={() => router.push("/notice")} gap="8px">
         <Title>공지사항</Title>
         <RightArrowIcon color={color.gray900} size={22} />
-      </NoticeButton>
+      </Link>
       <NoticeList>
         {MainNoticeItemData?.map((item) => (
           <NoticeItem title={item.title} id={item.id} />
@@ -32,12 +33,6 @@ const StyledNotice = styled.div`
   gap: 16px;
   width: 48%;
   height: 33%;
-`;
-
-const NoticeButton = styled.button`
-  display: flex;
-  align-items: center;
-  gap: 8px;
 `;
 
 const Title = styled.p`
