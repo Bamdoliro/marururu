af2b9795f56e44ba78fcfeddd303deab9b2e4181
SEOKKAMONI <sj01048260279@gmail.com>
2023-05-31 20:18:55 +0900

    ADD :: ScheduleItem Line

--- src/components/main/ScheduleItem.tsx
+++ src/components/main/ScheduleItem.tsx
@@ -12,6 +12,7 @@ const ScheduleItem = ({ date, plan }: PropsType) => {
   return (
     <StyledScheduleItem>
       <Date>{date}</Date>
+      <Line />
       <Row gap="6px" alignItems="center">
         <Bar />
         <Plan>{plan}</Plan>
@@ -30,11 +31,14 @@ const StyledScheduleItem = styled.div`
   width: 100%;
 `;
 
+const Line = styled.div`
+  width: 100%;
+  border: 0.5px solid ${color.gray300};
+`;
+
 const Date = styled.p`
   ${font.context}
   color: ${color.gray900};
-  border-bottom: 1px solid ${color.gray300};
-  padding-bottom: 3px;
 `;
 
 const Plan = styled.p`
