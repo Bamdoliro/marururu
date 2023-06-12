1b0a8fafe73a861858adbce5b2b07f1a22eac7b5
이명재 <arkk20000@gmail.com>
2023-06-13 02:41:48 +0900

    REFACTOR :: 리뷰 반영했습니다

--- src/components/main/ApplicationCard/ApplicationCard.tsx
+++ src/components/main/ApplicationCard/ApplicationCard.tsx
@@ -1,55 +1,59 @@
 import { font } from "@/styles/font";
 import { styled } from "styled-components";
-import moment from "moment";
 import { color } from "@/styles/color";
 import Column from "@/components/common/Flex/Column";
 import Button from "@/components/common/Button";
+import { formatDate, getRemainDays } from "@/utils/timer";
 
-interface ApplicationCardPropTypes {
-  period: string;
-  status: "모집 중" | "1차 합격자 발표" | "최종합격자 발표";
+export enum ApplicationStatus {
+  RECRUITING = "모집 중",
+  FIRST_PASS = "1차 합격자 발표",
+  FINAL_PASS = "최종합격자 발표",
 }
 
-const ApplicationCard = ({ period, status }: ApplicationCardPropTypes) => {
+interface ApplicationCardPropsType {
+  period: string;
+  status: ApplicationStatus;
+}
+
+const ApplicationCard = ({ period, status }: ApplicationCardPropsType) => {
   const isRecruiting = status === "모집 중";
-
-  const formatter = new Intl.DateTimeFormat("ko", {
-    dateStyle: "long",
-  });
-
-  const remainDays =
-    isRecruiting || Math.ceil(moment(period).diff(moment(), "days", true));
-  const convertedDate = isRecruiting || formatter.format(new Date(period));
 
   return (
     <StyledApplicationCard>
       <Column gap="0" width="100%" height="100%" justifyContent="space-between">
         {isRecruiting ? (
           <Column gap="36px">
-            <Announcement>
+            <Notice>
               부산소프트웨어마이스터고등학교
               <br />
               2024학년도 신입생 모집
-            </Announcement>
+            </Notice>
             <Period>{period}</Period>
           </Column>
         ) : (
           <Column gap="16px">
             <Column gap="8px">
-              <DDayGoal>{status}</DDayGoal>
-              <DDay>D-{remainDays}</DDay>
+              <Status>{status}</Status>
+              <RemainDays>D-{getRemainDays(period)}</RemainDays>
             </Column>
-            <Period>{convertedDate}</Period>
+            <Period>{formatDate(period)}</Period>
           </Column>
         )}
 
-        <Button
-          width="321px"
-          size="LARGE"
-          option={isRecruiting || remainDays === 0 ? "PRIMARY" : "DISABLED"}
-        >
-          {isRecruiting ? "원서 접수하기" : "결과 확인하기"}
-        </Button>
+        {isRecruiting ? (
+          <Button width="321px" size="LARGE">
+            원서 접수하기
+          </Button>
+        ) : (
+          <Button
+            width="321px"
+            size="LARGE"
+            option={getRemainDays(period) === 0 ? "PRIMARY" : "DISABLED"}
+          >
+            결과 확인하기
+          </Button>
+        )}
       </Column>
     </StyledApplicationCard>
   );
@@ -72,7 +76,7 @@ const StyledApplicationCard = styled.div`
   background-blend-mode: darken;
 `;
 
-const Announcement = styled.p`
+const Notice = styled.p`
   ${font.H1};
   color: ${color.white};
 `;
@@ -82,12 +86,12 @@ const Period = styled.p`
   color: ${color.gray300};
 `;
 
-const DDayGoal = styled.p`
+const Status = styled.p`
   ${font.H2}
   color: ${color.gray400};
 `;
 
-const DDay = styled.p`
+const RemainDays = styled.p`
   ${font.D1}
   color: ${color.white};
 `;
