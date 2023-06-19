85dda089aa3ae0c05e700b36f906e463d452c84a
SEOKKAMONI <sj01048260279@gmail.com>
2023-06-19 11:21:17 +0900

    ADD :: 리퍼블리싱

--- src/components/form/ProgressBar.tsx
+++ src/components/form/ProgressBar.tsx
@@ -1,16 +1,42 @@
 import { color } from "@/styles/color";
 import { font } from "@/styles/font";
 import styled from "styled-components";
-import Column from "../common/Flex/Column";
+
+const PROGRESS_BAR_DATA = [
+  {
+    id: 0,
+    name: "지원자 정보",
+  },
+  {
+    id: 1,
+    name: "보호자 정보",
+  },
+  {
+    id: 2,
+    name: "출신학교 및 학력",
+  },
+  {
+    id: 3,
+    name: "전형 선택",
+  },
+  {
+    id: 4,
+    name: "성적 입력",
+  },
+  {
+    id: 5,
+    name: "자기소개서",
+  },
+];
 
 const ProgressBar = () => {
   return (
     <StyledProgressBar>
-      <Column gap="4px" alignItems="center">
-        <Circle activate={false}>1</Circle>
-        <Name activate={false}>지원자 정보</Name>
-      </Column>
-      <Line />
+      {PROGRESS_BAR_DATA.map((item, index) => (
+        <Circle key={item.id} name={item.name} active={false}>
+          {index + 1}
+        </Circle>
+      ))}
     </StyledProgressBar>
   );
 };
@@ -18,17 +44,27 @@ const ProgressBar = () => {
 export default ProgressBar;
 
 const StyledProgressBar = styled.div`
+  position: relative;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   width: 100%;
-  height: 69px;
   padding: 0px 205px;
-  border: 1px solid black;
+  margin-bottom: 61px;
+  &::before {
+    position: absolute;
+    content: "";
+    width: calc(100% - 410px);
+    height: 2px;
+    background-color: ${color.gray300};
+  }
 `;
 
-const Circle = styled.div<{ activate: boolean }>`
+const Circle = styled.div<{ active: boolean; name: string }>`
+  z-index: 1;
   ${font.p1}
-  color: ${(props) => (props.activate ? color.maruDefault : color.gray300)};
+  color: ${(props) => (props.active ? color.maruDefault : color.gray600)};
+  background-color: ${color.gray50};
   display: flex;
   justify-content: center;
   align-items: center;
@@ -36,15 +72,13 @@ const Circle = styled.div<{ activate: boolean }>`
   width: 44px;
   height: 44px;
   border: 2px solid
-    ${(props) => (props.activate ? color.maruDefault : color.gray300)};
-`;
-
-const Name = styled.div<{ activate: boolean }>`
-  ${font.context}
-  color: ${(props) => (props.activate ? color.maruDefault : color.gray300)};
-`;
-
-const Line = styled.div`
-  width: 146.5px;
-  border: 1px solid ${color.gray300};
+    ${(props) => (props.active ? color.maruDefault : color.gray300)};
+  cursor: pointer;
+  &::after {
+    ${font.context}
+    color: ${(props) => (props.active ? color.maruDefault : color.gray600)};
+    content: "${(props) => props.name}";
+    position: absolute;
+    top: calc(100% + 4px);
+  }
 `;
