e090d51e7180729fce933ef861ed65a7393ea744
SEOKKAMONI <sj01048260279@gmail.com>
2023-06-20 10:58:50 +0900

    DELETE :: 다시 세팅

--- packages/global-style/src/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as color } from './color';
-export { default as font } from './font';
-export { default as global } from './global';
--- packages/ui/index.tsx
+++
@@ -1,6 +0,0 @@
-export { default as Button } from './components/Button';
-export { default as DropdownMenu } from './components/DropdownMenu';
-export { default as Row } from './components/Flex/Row';
-export { default as Column } from './components/Flex/Column';
-export { default as Input } from './components/Input';
-export { default as Link } from './components/Link';
