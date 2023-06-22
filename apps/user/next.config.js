3d4c5b0abb29d6446cc9497108ddbee6e018fad4
SEOKKAMONI <sj01048260279@gmail.com>
2023-06-22 13:59:58 +0900

    ADD :: configs 분리

--- apps/user/next.config.js
+++ apps/user/next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    transpilePackages: ['@maru/ui'],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
