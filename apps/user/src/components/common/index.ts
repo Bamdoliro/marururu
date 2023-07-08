f224dfd49efe43187aa8d3f83c81af1bb7197297
이명재 <arkk20000@gmail.com>
2023-07-08 20:20:48 +0900

    ADD :: components 폴더와 common, Icons 폴더에 index.ts 파일 추가

---
+++ apps/user/src/components/common/index.ts
@@ -0,0 +1,2 @@
+export { default as Footer } from './Footer/Footer';
+export { default as Header } from './Header/Header';
---
+++ apps/user/src/components/index.ts
@@ -0,0 +1,14 @@
+export { default as FaqItem } from './faq/FaqItem/FaqItem';
+export { default as CategoryFilter } from './faq/CategoryFilter/CategoryFilter';
+
+export { default as ProgressBar } from './form/ProgressBar/ProgressBar';
+
+export { default as Dday } from './main/Dday/Dday';
+export { default as Faq } from './main/Faq/Faq';
+export { default as Notice } from './main/Notice/Notice';
+export { default as Schedule } from './main/Schedule/Schedule';
+
+export { default as NoticeHeader } from './notice/NoticeHeader/NoticeHeader';
+export { default as NoticeItem } from './notice/NoticeItem/NoticeItem';
+
+export { default as Terms } from './signup/Terms/Terms';
