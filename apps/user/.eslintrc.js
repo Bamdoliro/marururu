d42e596418161714cfc4f57624a9b1aa1b6d4470
SEOKKAMONI <sj01048260279@gmail.com>
2023-07-10 12:09:00 +0900

    FIX :: common naming

--- apps/user/.eslintrc.js
+++ apps/user/.eslintrc.js
@@ -1,10 +1,5 @@
 module.exports = {
     root: true,
     // This tells ESLint to load the config from the package `eslint-config-custom`
-    extends: ['@maru/eslint/custom', '@maru/eslint/query'],
-    settings: {
-        next: {
-            rootDir: ['apps/*/'],
-        },
-    },
+    extends: ['@maru/eslint/common, @maru/eslint/query'],
 };
