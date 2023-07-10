bae994ebcc0ba53667adac755e0dd5f5c389c2cb
SEOKKAMONI <sj01048260279@gmail.com>
2023-07-10 11:17:24 +0900

    FIX :: package 에러 수정

--- packages/maru-utils/functions/formatTime.ts
+++ packages/maru-utils/functions/formatTime.ts
@@ -1,8 +1,6 @@
 const formatTime = (time: number) => {
-    const minutes = Math.floor(time / 60)
-        .toString()
-        .padStart(2, '0');
-    const seconds = (time % 60).toString().padStart(2, '0');
+    const minutes = ('0' + Math.floor(time / 60)).slice(-2);
+    const seconds = ('0' + (time % 60)).slice(-2);
 
     return `${minutes}:${seconds}`;
 };
