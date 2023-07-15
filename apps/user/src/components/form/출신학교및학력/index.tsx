09dcf2b2d24657d86b0e60b225535680f3e88bc8
이명재 <arkk20000@gmail.com>
2023-07-16 07:40:48 +0900

    FIX :: 출신학교 및 학력 페이지 디자인에 맞게 다시 수정

--- apps/user/src/components/form/출신학교및학력/index.tsx
+++ apps/user/src/components/form/출신학교및학력/index.tsx
@@ -9,6 +9,12 @@ const 출신학교및학력 = () => {
 
     return (
         <Styled출신학교및학력>
+            <RadioGroup
+                label="졸업 구분"
+                name="gradurationStatus"
+                list={['졸업 예정', '졸업', '고입 검정']}
+            />
+            <div></div>
             <ButtonInput
                 name="almaMater"
                 label="출신학교"
@@ -18,16 +24,12 @@ const 출신학교및학력 = () => {
                 placeholder="클릭하여 검색하기"
                 readOnly
             />
-            <Input name="neisNumber" label="학교 나이스번호" placeholder="뭐시기 뭐시기" />
-            <RadioGroup
-                label="졸업 구분"
-                name="gradurationStatus"
-                list={['졸업 예정', '졸업', '고입 검정']}
-            />
+            <div></div>
             <Input
                 name="gradurationYear"
                 label="졸업 년도, 합격 년도"
                 placeholder="뭐시기 뭐시기"
+                width="100%"
             />
             <ButtonInput
                 name="regions"
@@ -38,9 +40,30 @@ const 출신학교및학력 = () => {
                 placeholder="도로명 주소"
                 readOnly
             />
-            <div></div>
-            <Input name="teacherName" label="작성 교사 이름" placeholder="뭐시기 뭐시기" />
-            <Input name="teacherPhone" label="작성 교사 연락처" placeholder="뭐시기 뭐시기" />
+            <Input
+                name="neisNumber"
+                label="학교 나이스번호"
+                placeholder="뭐시기 뭐시기"
+                width="100%"
+            />
+            <Input
+                name="schoolContact"
+                label="학교 연락처"
+                placeholder="뭐시기 뭐시기"
+                width="100%"
+            />
+            <Input
+                name="teacherName"
+                label="작성 교사 이름"
+                placeholder="뭐시기 뭐시기"
+                width="100%"
+            />
+            <Input
+                name="teacherContact"
+                label="작성 교사 연락처"
+                placeholder="뭐시기 뭐시기"
+                width="100%"
+            />
             {isOpen && <SchoolSearchModal closeModal={closeModal} />}
         </Styled출신학교및학력>
     );
