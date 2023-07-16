a577bde2f1f48d03479bb5eba3449fee0bd1265a
SEOKKAMONI <sj01048260279@gmail.com>
2023-07-16 17:36:43 +0900

    ADD :: AddressModal

---
+++ apps/user/src/components/form/보호자정보/AddressModal/AddressModal.tsx
@@ -0,0 +1,40 @@
+import DaumPostcode from 'react-daum-postcode';
+import { useOutsideClick } from '@maru/hooks';
+import styled from 'styled-components';
+
+interface PropsType {
+    isOpenAddressModal: boolean;
+    isClose: () => void;
+}
+
+const AddressModal = ({ isOpenAddressModal, isClose }: PropsType) => {
+    const addressModalRef = useOutsideClick(isClose);
+
+    return (
+        <Background isOpen={isOpenAddressModal}>
+            <StyledAddressModal ref={addressModalRef}>
+                <DaumPostcode style={{ width: '100%', height: '100%' }} />
+            </StyledAddressModal>
+        </Background>
+    );
+};
+
+export default AddressModal;
+
+const Background = styled.div<{ isOpen: boolean }>`
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: ${(props) => (props.isOpen ? 'flex' : 'none')};
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.4);
+    z-index: 1;
+`;
+
+const StyledAddressModal = styled.div`
+    width: 500px;
+    height: 500px;
+`;
--- apps/user/src/components/form/보호자정보/보호자정보.tsx
+++ apps/user/src/components/form/보호자정보/보호자정보.tsx
@@ -1,23 +1,37 @@
 import { ButtonInput, Column, Input, Row } from '@maru/ui';
+import { useState } from 'react';
 import styled from 'styled-components';
+import AddressModal from './AddressModal/AddressModal';
 
 const 보호자정보 = () => {
+    const [isOpenAddressModal, setIsOpenAddressModal] = useState(false);
+
+    const openAddressModal = () => {
+        setIsOpenAddressModal(true);
+    };
+
     return (
-        <Styled보호자정보>
-            <Column gap={30}>
-                <Row gap={48} alignItems="center">
-                    <Input label="성명" width="100%" />
-                    <Input label="전화번호" placeholder="- 없이 입력" width="100%" />
-                </Row>
-                <ButtonInput
-                    label="주소"
-                    buttonText="검색"
-                    handleButtonClick={() => console.log('검색 입력 창입니다')}
-                    width="100%"
-                />
-                <Input label="상세 주소" width="100%" />
-            </Column>
-        </Styled보호자정보>
+        <>
+            <Styled보호자정보>
+                <Column gap={30}>
+                    <Row gap={48} alignItems="center">
+                        <Input label="성명" width="100%" />
+                        <Input label="전화번호" placeholder="- 없이 입력" width="100%" />
+                    </Row>
+                    <ButtonInput
+                        label="주소"
+                        buttonText="검색"
+                        handleButtonClick={openAddressModal}
+                        width="100%"
+                    />
+                    <Input label="상세 주소" width="100%" />
+                </Column>
+            </Styled보호자정보>
+            <AddressModal
+                isOpenAddressModal={isOpenAddressModal}
+                isClose={() => setIsOpenAddressModal(false)}
+            />
+        </>
     );
 };
 
