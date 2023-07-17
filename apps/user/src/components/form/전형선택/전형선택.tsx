95825b988768d905f742bcbf3a2ba78ac26f3d75
이명재 <arkk20000@gmail.com>
2023-07-17 12:55:32 +0900

    FIX :: 깃허브 리뷰 반영

--- apps/user/src/components/form/전형선택/전형선택.tsx
+++ apps/user/src/components/form/전형선택/전형선택.tsx
@@ -4,62 +4,62 @@ import { styled } from 'styled-components';
 
 const 전형선택 = () => {
     const [selectedAdmissions, setSelectedAdmissions] = useState({
-        admissions: '',
-        specialAdmissions: '',
-        equalOpportunityAdmissions: '',
-        diversityAdmissions: '',
+        입학전형선택: '',
+        특별전형선택: '',
+        기회균등전형선택: '',
+        사회다양성전형선택: '',
     });
 
-    const handleRadioChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-        if (e.target.name === 'specialAdmissions') {
+    const handleAdmissionsChange: ChangeEventHandler<HTMLInputElement> = ({
+        target: { name, value },
+    }) => {
+        if (name === '특별전형선택') {
             setSelectedAdmissions({
                 ...selectedAdmissions,
-                specialAdmissions: e.target.value,
-                equalOpportunityAdmissions: '',
-                diversityAdmissions: '',
+                특별전형선택: value,
+                기회균등전형선택: '',
+                사회다양성전형선택: '',
             });
-        } else if (e.target.name === 'admissions') {
+        } else if (name === '입하전형선택') {
             setSelectedAdmissions({
-                admissions: e.target.value,
-                specialAdmissions: '',
-                equalOpportunityAdmissions: '',
-                diversityAdmissions: '',
+                입학전형선택: value,
+                특별전형선택: '',
+                기회균등전형선택: '',
+                사회다양성전형선택: '',
             });
         } else {
             setSelectedAdmissions({
                 ...selectedAdmissions,
-                [e.target.name]: e.target.value,
+                [name]: value,
             });
         }
     };
-
-    console.log(JSON.stringify(selectedAdmissions, null, 2));
 
     return (
         <Styled전형선택>
             <RadioGroup
                 label="입학 전형 선택"
-                name="admissions"
+                name="입하전형선택"
                 list={['일반전형', '특별전형']}
-                onChange={handleRadioChange}
+                onChange={handleAdmissionsChange}
             />
-            {selectedAdmissions.admissions === '특별전형' && (
+            {selectedAdmissions.입학전형선택 === '특별전형' && (
                 <RadioGroup
                     label="특별 전형 선택"
-                    name="specialAdmissions"
+                    name="특별전형선택"
                     list={[
                         '마이스터인재정형',
                         '기회균등전형',
                         '사회다양성전형',
                         '특례입학대상자전형',
                     ]}
-                    onChange={handleRadioChange}
+                    onChange={handleAdmissionsChange}
                 />
             )}
-            {selectedAdmissions.specialAdmissions === '기회균등전형' && (
+            {selectedAdmissions.특별전형선택 === '기회균등전형' && (
                 <RadioGroup
                     label="기회 균등 전형 선택"
-                    name="equalOpportunityAdmissions"
+                    name="기회균등전형선택"
                     list={[
                         '국민기초생활수급권자',
                         '차상위계층',
@@ -67,13 +67,13 @@ const 전형선택 = () => {
                         '한부모가정',
                         '북한이탈청소년',
                     ]}
-                    onChange={handleRadioChange}
+                    onChange={handleAdmissionsChange}
                 />
             )}
-            {selectedAdmissions.specialAdmissions === '사회다양성전형' && (
+            {selectedAdmissions.특별전형선택 === '사회다양성전형' && (
                 <RadioGroup
                     label="사회다양성 전형 선택"
-                    name="diversityAdmissions"
+                    name="사회다양성전형선택"
                     list={[
                         '다문화가정',
                         '차상위계층',
@@ -81,7 +81,7 @@ const 전형선택 = () => {
                         '한부모가정',
                         '북한이탈청소년',
                     ]}
-                    onChange={handleRadioChange}
+                    onChange={handleAdmissionsChange}
                 />
             )}
         </Styled전형선택>
