388390c7923eb92488b897ec61f837d4846d9290
이명재 <arkk20000@gmail.com>
2023-07-18 12:09:56 +0900

    FIX :: 깃허브 리뷰 반영

--- apps/user/src/components/form/성적입력/성적입력.tsx
+++ apps/user/src/components/form/성적입력/성적입력.tsx
@@ -15,11 +15,11 @@ const 성적입력 = ({ onPrevious, onNext }: PropsType) => {
 
     return (
         <FormLayout title="성적 입력">
-            <Caution style={{ marginBottom: 24 }}>
+            <Desc style={{ marginBottom: 24 }}>
                 *교과성적이 없는 학기나 학년의 경우 모집요강을 반드시 확인 바랍니다.
                 <br />
                 *성취수준이 없고 원점수로 되어있는 학기나 학년은 아래표를 참고 바랍니다.
-            </Caution>
+            </Desc>
             <div>모의 성적 계산</div>
             <NavigationBar>
                 <Button
@@ -49,37 +49,35 @@ const 성적입력 = ({ onPrevious, onNext }: PropsType) => {
             </NavigationBar>
             {fieldStep === '성적 입력' && (
                 <>
-                    <Caution>
+                    <Desc>
                         *교과성적이 없는 학기나 학년의 경우 모집요강을 반드시 확인 바랍니다.
                         <br />
                         *성취수준이 없고 원점수로 되어있는 학기나 학년은 아래표를 참고 바랍니다.
-                    </Caution>
+                    </Desc>
                     <div>성적 입력 테이블</div>
                 </>
             )}
             {fieldStep === '출결상황' && (
                 <>
-                    <Caution>
+                    <Desc>
                         *2023.09.30까지의 출결상황을 기재해주세요. 졸업생은 졸업일 기준으로
                         기재해주세요.
-                    </Caution>
+                    </Desc>
                     <div>출결상황 입력 테이블</div>
                 </>
             )}
             {fieldStep === '봉사시간' && (
                 <>
-                    <Caution>
+                    <Desc>
                         *2023.09.30까지의 봉사시간을 기재해주세요. 졸업생은 졸업일 기준으로
                         기재해주세요.
-                    </Caution>
+                    </Desc>
                     <div>봉사시간 입력 테이블</div>
                 </>
             )}
             {fieldStep === '자격증' && (
                 <>
-                    <Caution>
-                        *자격증을 중복 소지하고 있을 경우, 최고 수준의 자격증 1개만 인정함
-                    </Caution>
+                    <Desc>*자격증을 중복 소지하고 있을 경우, 최고 수준의 자격증 1개만 인정함</Desc>
                     <div>자격증 입력 테이블</div>
                 </>
             )}
@@ -89,7 +87,7 @@ const 성적입력 = ({ onPrevious, onNext }: PropsType) => {
 
 export default 성적입력;
 
-const Caution = styled.p`
+const Desc = styled.p`
     color: ${color.red};
     ${font.p3}
     margin-bottom: 16px;
@@ -99,7 +97,6 @@ const NavigationBar = styled.div`
     ${flex({ alignItems: 'center' })}
     width: 100%;
     height: 54px;
-    margin-top: 64px;
-    margin-bottom: 16px;
+    margin: 64px 0 16px;
     background-color: ${color.white};
 `;
