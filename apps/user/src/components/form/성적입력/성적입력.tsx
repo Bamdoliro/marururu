c38035da8b3c71c8b82d099707e44d1d36019ffb
이명재 <arkk20000@gmail.com>
2023-07-22 11:41:22 +0900

    ADD :: 봉사시간 테이블 퍼블리싱

--- apps/user/src/components/form/성적입력/성적입력.tsx
+++ apps/user/src/components/form/성적입력/성적입력.tsx
@@ -2,6 +2,7 @@ import { FormLayout } from '@/layouts';
 import GradeCalculator from './GradeCalculator/GradeCalculator';
 import GradePreview from './GradePreview/GradePreview';
 import AttendanceCalculator from './AttendanceCalculator/AttendanceCalculator';
+import VolunteerCalculator from './VolunteerCalculator/VolunteerCalculator';
 
 interface PropsType {
     onPrevious: () => void;
@@ -13,7 +14,8 @@ const 성적입력 = ({ onPrevious, onNext }: PropsType) => {
         <FormLayout title="성적 입력">
             <GradePreview />
             {/* <GradeCalculator /> */}
-            <AttendanceCalculator />
+            {/* <AttendanceCalculator /> */}
+            <VolunteerCalculator />
         </FormLayout>
     );
 };
