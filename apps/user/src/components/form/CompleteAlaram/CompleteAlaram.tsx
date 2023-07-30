d6813e234af75d41d20192a7450d29433c2792ef
SEOKKAMONI <sj01048260279@gmail.com>
2023-07-30 21:28:09 +0900

    FIX :: CompleteAlaram

--- apps/user/src/components/form/CompleteAlaram/CompleteAlaram.tsx
+++ apps/user/src/components/form/CompleteAlaram/CompleteAlaram.tsx
@@ -7,14 +7,17 @@ import styled from 'styled-components';
 const CompleteAlaram = () => {
     const complete = true;
     return (
-        <AppLayout header={true} style={{ marginTop: '173px' }}>
-            <Column width="100%" height="100%" gap={34} alignItems="center">
-                {complete ? <CheckCircleIcon /> : <CancelCircleIcon />}
-                <AlertMessage>
-                    {complete ? <p>원서 작성 완료!</p> : <p>아직 작성하지 않은 곳이 있어요</p>}
-                </AlertMessage>
-            </Column>
-        </AppLayout>
+        <Column
+            style={{ marginTop: '173px' }}
+            width="100%"
+            height="100%"
+            gap={34}
+            alignItems="center">
+            {complete ? <CheckCircleIcon size={150} /> : <CancelCircleIcon size={150} />}
+            <AlertMessage>
+                {complete ? <p>원서 작성 완료!</p> : <p>아직 작성하지 않은 곳이 있어요</p>}
+            </AlertMessage>
+        </Column>
     );
 };
 
