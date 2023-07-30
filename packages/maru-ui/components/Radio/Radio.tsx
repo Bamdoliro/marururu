0a4941b6098e3b0c27f9f6afa188111ddf8a53fc
이명재 <arkk20000@gmail.com>
2023-07-30 20:18:56 +0900

    FIX :: 라디오 컴포넌트에 RadioSelectIcon, 네이티브 아이콘으로 대체했습니다

--- packages/maru-ui/components/Radio/Radio.tsx
+++ packages/maru-ui/components/Radio/Radio.tsx
@@ -15,59 +15,29 @@ const Radio = ({ content, value, name, defaultChecked, onChange }: RadioPropsTyp
                     defaultChecked={defaultChecked}
                     onChange={onChange}
                 />
-                <RadioSelectIcon />
                 {content && <Content>{content}</Content>}
             </Label>
         </div>
     );
 };
 
-/* TODO : 이거 나중에 아이콘으로 교체해야하는데 생각 좀 해봐야할 듯 */
-const RadioSelectIcon = styled.div`
-    position: relative;
+const Input = styled.input`
     width: 20px;
     height: 20px;
-    margin: 2px;
-    border: 1px solid ${color.gray400};
-    border-radius: 50%;
-`;
-
-const Input = styled.input`
-    display: none;
-
-    &:checked + ${RadioSelectIcon} {
-        border: 2px solid ${color.maruDefault};
-    }
-
-    &:checked + ${RadioSelectIcon}::after {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        display: block;
-        content: '';
-        width: 10px;
-        height: 10px;
-        border-radius: 50%;
-        transform: translate(-50%, -50%);
-        background-color: ${color.maruDefault};
-    }
 `;
 
 const Label = styled.label`
     display: flex;
     align-items: center;
     gap: 8px;
-    height: 26px;
-
-    &:hover ${RadioSelectIcon} {
-        border-color: ${color.gray600};
-    }
+    min-height: 24px;
 `;
 
 const Content = styled.p`
     ${font.p2};
     color: ${color.gray900};
     margin-right: 40px;
+    height: 26px;
 `;
 
 export default Radio;
