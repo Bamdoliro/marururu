f4069d2981b590837fbae464d402064a1d045747
이명재 <arkk20000@gmail.com>
2023-08-02 05:28:53 +0900

    FIX :: 전형 선택 페이지 데이터 유지하게 수정

--- apps/user/src/app/form/전형선택/전형선택.tsx
+++ apps/user/src/app/form/전형선택/전형선택.tsx
@@ -1,10 +1,10 @@
+import { FormController } from '@/components/form';
 import { FormLayout } from '@/layouts';
 import { RadioGroup } from '@maru/ui';
 import { flex } from '@maru/utils';
-import { useInput, useCTAButton } from './전형선택.hooks';
-import { FormController } from '@/components/form';
-import { useChoiceFormTypeState } from './전형선택.state';
 import { styled } from 'styled-components';
+import { useCTAButton, useInput } from './전형선택.hooks';
+import { useChoiceFormTypeState } from './전형선택.state';
 
 const 전형선택 = () => {
     const { choiceFormType } = useChoiceFormTypeState();
@@ -18,6 +18,7 @@ const 전형선택 = () => {
                     label="입학 전형 선택"
                     name="입학전형선택"
                     list={['일반전형', '특별전형']}
+                    value={choiceFormType.입학전형선택}
                     onChange={handleFormTypeDataChange}
                 />
                 {choiceFormType.입학전형선택 === '특별전형' && (
@@ -30,6 +31,7 @@ const 전형선택 = () => {
                             '사회다양성전형',
                             '특례입학대상자전형',
                         ]}
+                        value={choiceFormType.특별전형선택}
                         onChange={handleFormTypeDataChange}
                     />
                 )}
@@ -44,6 +46,7 @@ const 전형선택 = () => {
                             '한부모가정',
                             '북한이탈주민',
                         ]}
+                        value={choiceFormType.기회균등전형선택}
                         onChange={handleFormTypeDataChange}
                     />
                 )}
@@ -51,13 +54,8 @@ const 전형선택 = () => {
                     <RadioGroup
                         label="사회다양성 전형 선택"
                         name="사회다양성전형선택"
-                        list={[
-                            '다문화가정',
-                            '차상위계층',
-                            '국가보훈자녀',
-                            '한부모가정',
-                            '북한이탈주민',
-                        ]}
+                        list={['다문화가정', '소년소녀가장', '다자녀가정자녀', '농어촌지역출신자']}
+                        value={choiceFormType.사회다양성전형선택}
                         onChange={handleFormTypeDataChange}
                     />
                 )}
