cc63a916c7fb5e5730456ef7a2cf61f107294db4
SEOKKAMONI <sj01048260279@gmail.com>
2023-08-03 00:40:37 +0900

    FIX :: 충돌 해결

--- apps/user/src/services/form/출신학교및학력/api.ts
+++
@@ -1,7 +0,0 @@
-import { maru } from '@/apis/instance/instance';
-
-export const getSchoolList = async (school: string) => {
-    const { data } = await maru.get(`/school?q=${school}`);
-
-    return data;
-};
