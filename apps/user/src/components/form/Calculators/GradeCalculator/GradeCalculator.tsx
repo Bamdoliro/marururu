08a1e75917dcf7f5e63b33d6abff69b0b0be3846
SEOKKAMONI <sj01048260279@gmail.com>
2023-08-07 22:26:07 +0900

    FIX :: 경로 변경

--- apps/user/src/components/form/Calculators/GradeCalculator/GradeCalculator.tsx
+++ apps/user/src/components/form/Calculators/GradeCalculator/GradeCalculator.tsx
@@ -1,9 +1,9 @@
 import { color } from '@maru/theme';
 import { Button } from '@maru/ui';
 import { flex } from '@maru/utils';
-import GradeCalculatorHeader from '../GradeCalculatorHeader/GradeCalculatorHeader';
-import NewGradeCalculatorItem from '../NewGradeCalculatorItem/NewGradeCalculatorItem';
-import GradeCalculatorItem from '../GradeCalculatorItem/GradeCalculatorItem';
+import GradeCalculatorHeader from './GradeCalculatorHeader/GradeCalculatorHeader';
+import NewGradeCalculatorItem from './NewGradeCalculatorItem/NewGradeCalculatorItem';
+import GradeCalculatorItem from './GradeCalculatorItem/GradeCalculatorItem';
 import { Dispatch, SetStateAction, useEffect, useRef } from 'react';
 import { Subject } from '@/types/form/client';
 import styled from 'styled-components';
