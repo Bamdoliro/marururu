4ddfe13b96f734754efb5185f8dda03eaab227ad
SEOKKAMONI <sj01048260279@gmail.com>
2023-08-12 15:38:59 +0900

    ADD :: 이러면 한번만 쏨

--- apps/user/src/apis/token/refresh.ts
+++ apps/user/src/apis/token/refresh.ts
@@ -7,14 +7,14 @@ const refreshToken = async () => {
     try {
         const { data } = await maru.patch('/auth', null, {
             headers: {
-                'Refresh-Token': `${Storage.getItem(TOKEN.REFRESH)}`,
+                'Refresh-Token': `${Storage.getItem('refresh-token')}`,
             },
         });
         Storage.setItem(TOKEN.ACCESS, data.accessToken);
     } catch {
+        window.location.href = ROUTES.LOGIN;
         alert('다시 로그인 해주세요');
         localStorage.clear();
-        window.location.href = ROUTES.LOGIN;
     }
 };
 
