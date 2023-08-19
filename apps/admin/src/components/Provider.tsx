62123afd88cb4d61fd15ac46e256fbfb93dbf8bc
이명재 <arkk20000@gmail.com>
2023-08-19 11:06:11 +0900

    ADD :: Provider 컴포넌트 제작

---
+++ apps/admin/src/components/Provider.tsx
@@ -0,0 +1,17 @@
+import { GlobalStyle } from '@maru/theme';
+import { ReactNode } from 'react';
+
+interface PropsType {
+    children: ReactNode;
+}
+
+const Provider = ({ children }: PropsType) => {
+    return (
+        <>
+            <GlobalStyle />
+            {children}
+        </>
+    );
+};
+
+export default Provider;
