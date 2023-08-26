95d70752b206a73935577fd6e9dc5217a24be769
SEOKKAMONI <sj01048260279@gmail.com>
2023-08-27 01:16:22 +0900

    ADD :: GuidelineBox

--- apps/user/src/components/main/index.ts
+++ apps/user/src/components/main/index.ts
@@ -1,4 +1,5 @@
 export { default as Dday } from './Dday/Dday';
 export { default as Faq } from './Faq/MainFaq';
+export { default as GuidelineBox } from './GuidelineBox/GuidelineBox';
 export { default as Notice } from './Notice/MainNotice';
 export { default as Schedule } from './Schedule/Schedule';
