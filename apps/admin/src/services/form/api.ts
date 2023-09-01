e5c164164cce6fc544eac8358dee6c1f4c3f350a
이명재 <arkk20000@gmail.com>
2023-09-02 01:23:07 +0900

    FIX :: 빠진 authorization 호출 추가

--- apps/admin/src/services/form/api.ts
+++ apps/admin/src/services/form/api.ts
@@ -1,8 +1,9 @@
 import { maru } from '@/apis/instance/instance';
+import { authorization } from '@/apis/token';
 import { GetFormReviewListRes } from '@/types/main/remote';
 
 export const getFormReviewList = async () => {
-    const { data } = await maru.get<GetFormReviewListRes>('/form/review');
+    const { data } = await maru.get<GetFormReviewListRes>('/form/review', authorization());
 
     return data;
 };
