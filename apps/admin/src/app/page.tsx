69cc3cf0e3d59a1528032d60fbc58d0bda1ac657
이명재 <arkk20000@gmail.com>
2023-09-14 01:58:47 +0900

    ADD :: 원서 조회 페이지 퍼블리싱

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,9 +1,13 @@
 'use client';
 
+import ButtonMenu from '@/components/common/ButtonMenu/ButtonMenu';
+import ButtonMenuItem from '@/components/common/ButtonMenu/ButtonMenuItem/ButtonMenuItem';
 import FormList from '@/components/main/FormList/FormList';
 import AppLayout from '@/layouts/AppLayout';
 import initMockAPI from '@/mocks';
-import { Button, Column, Row, SearchInput, Text } from '@maru/ui';
+import { IconCheckDocument, IconEditDocument, IconPrint, IconUpload } from '@maru/icon';
+import { color } from '@maru/theme';
+import { Column, Row, SearchInput, Text } from '@maru/ui';
 import { flex } from '@maru/utils';
 import { styled } from 'styled-components';
 
@@ -11,20 +15,51 @@ if (process.env.NODE_ENV === 'development') {
     initMockAPI();
 }
 
-const Home = () => {
+const MainPage = () => {
     return (
         <AppLayout>
             <StyledHome>
                 <Text fontType="H1">원서 관리</Text>
                 <Column gap={36}>
                     <Row justifyContent="space-between">
-                        <Button size="SMALL" icon="ADD_ICON">
-                            공지사항 작성
-                        </Button>
-                        <Row gap={8}>
-                            <SearchInput placeholder="검색어를 입력하세요." />
-                            <Button size="SMALL">검색</Button>
-                        </Row>
+                        <SearchInput placeholder="통합검색" />
+                        <ButtonMenu
+                            width={280}
+                            menuItemList={[
+                                <ButtonMenuItem>
+                                    <IconCheckDocument
+                                        color={color.gray600}
+                                        width={24}
+                                        height={24}
+                                    />
+                                    <Text fontType="p2" color={color.gray900}>
+                                        검토해야 하는 원서 모아보기
+                                    </Text>
+                                </ButtonMenuItem>,
+                                <ButtonMenuItem>
+                                    <IconEditDocument
+                                        color={color.gray600}
+                                        width={24}
+                                        height={24}
+                                    />
+                                    <Text fontType="p2" color={color.gray900}>
+                                        2차 전형 점수 입력하기
+                                    </Text>
+                                </ButtonMenuItem>,
+                                <ButtonMenuItem>
+                                    <IconUpload color={color.gray600} width={24} height={24} />
+                                    <Text fontType="p2" color={color.gray900}>
+                                        명단 엑셀로 내보내기
+                                    </Text>
+                                </ButtonMenuItem>,
+                                <ButtonMenuItem>
+                                    <IconPrint color={color.gray600} width={24} height={24} />
+                                    <Text fontType="p2" color={color.gray900}>
+                                        원서 출력하기
+                                    </Text>
+                                </ButtonMenuItem>,
+                            ]}
+                        />
                     </Row>
                     <FormList />
                 </Column>
@@ -33,7 +68,7 @@ const Home = () => {
     );
 };
 
-export default Home;
+export default MainPage;
 
 const StyledHome = styled.div`
     ${flex({ flexDirection: 'column' })}
