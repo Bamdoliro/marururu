78ff1b6871e9eac361b268a77ffb7121f0fea991
이명재 <arkk20000@gmail.com>
2023-09-17 00:15:18 +0900

    ADD :: alert -> 토스트

--- apps/admin/src/services/notice/mutations.ts
+++ apps/admin/src/services/notice/mutations.ts
@@ -3,6 +3,8 @@ import { useApiError } from '@/hooks';
 import { PostNoticeReq } from '@/types/notice/remote';
 import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { postNotice } from './api';
 
 export const useNoticePostMutation = ({ title, content }: PostNoticeReq) => {
@@ -12,7 +14,9 @@ export const useNoticePostMutation = ({ title, content }: PostNoticeReq) => {
     const { mutate: noticePostMutate, ...restMutation } = useMutation({
         mutationFn: () => postNotice({ title, content }),
         onSuccess: ({ data }) => {
-            alert('공지사항 작성이 완료되었습니다');
+            toast('업로드 완료', {
+                type: 'success',
+            });
             router.push(`${ROUTES.NOTICE}/${data.id}`);
         },
         onError: handleError,
