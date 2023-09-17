04345687ca95ca256762063d0d136bf4501d817b
SEOKKAMONI <sj01048260279@gmail.com>
2023-09-17 18:59:32 +0900

    ADD :: dayjs를 통한 active 기능

--- apps/user/src/components/main/Schedule/Schedule.tsx
+++ apps/user/src/components/main/Schedule/Schedule.tsx
@@ -1,3 +1,12 @@
+import {
+    이차_전형_끝,
+    이차_전형_시작,
+    일차_합격_발표,
+    제출_마감_날짜,
+    제출_시작_날짜,
+    최종_합격_발표,
+    최종_합격_발표_끝,
+} from '@/constants/form/constant';
 import { color } from '@maru/theme';
 import { Text } from '@maru/ui';
 import { flex } from '@maru/utils';
@@ -9,21 +18,29 @@ const SCHEDULE_DATA = [
         id: 0,
         date: '2023년 10월 16일 ~ 19일 17:00',
         plan: '1차 원서접수',
+        startTime: 제출_시작_날짜,
+        endTime: 제출_마감_날짜,
     },
     {
         id: 1,
         date: '2023년 10월 23일 15:00',
         plan: '1차 합격자 발표',
+        startTime: 일차_합격_발표,
+        endTime: 이차_전형_시작,
     },
     {
         id: 2,
-        date: '2023년 10월 17일 09:30 ~ 13:00',
+        date: '2023년 10월 27일 09:30 ~ 13:00',
         plan: '2차 전형',
+        startTime: 이차_전형_시작,
+        endTime: 이차_전형_끝,
     },
     {
         id: 3,
         date: '2023년 11월 2일 15:00',
         plan: '최종 합격자 발표',
+        startTime: 최종_합격_발표,
+        endTime: 최종_합격_발표_끝,
     },
 ] as const;
 
@@ -35,7 +52,13 @@ const Schedule = () => {
             </Text>
             <StyledScheduleList>
                 {SCHEDULE_DATA.map((item) => (
-                    <ScheduleItem key={item.id} date={item.date} plan={item.plan} />
+                    <ScheduleItem
+                        key={item.id}
+                        date={item.date}
+                        plan={item.plan}
+                        startTime={item.startTime}
+                        endTime={item.endTime}
+                    />
                 ))}
             </StyledScheduleList>
         </StyledSchedule>
--- apps/user/src/components/main/Schedule/ScheduleItem/ScheduleItem.tsx
+++ apps/user/src/components/main/Schedule/ScheduleItem/ScheduleItem.tsx
@@ -1,17 +1,27 @@
 import { color } from '@maru/theme';
 import { Text } from '@maru/ui';
 import { flex } from '@maru/utils';
-import styled from 'styled-components';
+import dayjs, { Dayjs } from 'dayjs';
+import isBetween from 'dayjs/plugin/isBetween';
+import utc from 'dayjs/plugin/utc';
+import styled, { css } from 'styled-components';
+
+dayjs.extend(isBetween);
+dayjs.extend(utc);
 
 interface Props {
     plan: string;
     date: string;
+    startTime: Dayjs;
+    endTime: Dayjs;
 }
 
-const ScheduleItem = ({ plan, date }: Props) => {
+const ScheduleItem = ({ plan, date, startTime, endTime }: Props) => {
+    const active = dayjs().isBetween(startTime, endTime);
+
     return (
-        <StyledScheduleItem>
-            <Text fontType="H5" color={color.gray900}>
+        <StyledScheduleItem active={active}>
+            <Text fontType="H5" color={active ? color.maruDefault : color.gray900}>
                 {plan}
             </Text>
             <Text fontType="p2" color={color.gray700}>
@@ -23,11 +33,16 @@ const ScheduleItem = ({ plan, date }: Props) => {
 
 export default ScheduleItem;
 
-const StyledScheduleItem = styled.div`
+const StyledScheduleItem = styled.div<{ active: boolean }>`
     ${flex({ alignItems: 'center', justifyContent: 'space-between' })}
     padding: 0px 20px;
     height: 64px;
     width: 100%;
     border-radius: 12px;
     background-color: ${color.gray50};
+    ${(props) =>
+        props.active &&
+        css`
+            border: 1px solid ${color.maruDefault};
+        `}
 `;
