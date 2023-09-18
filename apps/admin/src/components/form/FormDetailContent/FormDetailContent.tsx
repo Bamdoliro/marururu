1fc122ac0a67beeb3c51bf3cb0cc4d9112474e9e
이명재 <arkk20000@gmail.com>
2023-09-19 04:37:37 +0900

    ADD :: FormDetailContent 네비게이션

--- apps/admin/src/components/form/FormDetailContent/FormDetailContent.tsx
+++ apps/admin/src/components/form/FormDetailContent/FormDetailContent.tsx
@@ -1,4 +1,8 @@
-import { Column, Row } from '@maru/ui';
+import { FORM_DETAIL_STEP_LIST } from '@/constants/form/data';
+import { color } from '@maru/theme';
+import { Column, Row, UnderlineButton } from '@maru/ui';
+import { flex } from '@maru/utils';
+import { useState } from 'react';
 import styled from 'styled-components';
 import FormStatus from './FormStatus/FormStatus';
 import Profile from './Profile/Profile';
@@ -8,12 +12,30 @@ interface Props {
 }
 
 const FormDetailContent = ({ id }: Props) => {
+    const [currentFormDetailStep, setCurrentFormDetailStep] = useState('지원자 정보');
+
+    const handleFormDetailStepButtonClick = (formDetailStep: string) => {
+        setCurrentFormDetailStep(formDetailStep);
+    };
+
     return (
         <StyledFormDetailContent>
             <Row gap={48}>
                 <Column gap={36}>
                     <Profile id={id} />
                     <FormStatus id={id} />
+                </Column>
+                <Column gap={48}>
+                    <NavigationBar>
+                        {FORM_DETAIL_STEP_LIST.map((formDetailStep, index) => (
+                            <UnderlineButton
+                                key={`form-detail-step ${index}`}
+                                active={formDetailStep === currentFormDetailStep}
+                                onClick={() => handleFormDetailStepButtonClick(formDetailStep)}>
+                                {formDetailStep}
+                            </UnderlineButton>
+                        ))}
+                    </NavigationBar>
                 </Column>
             </Row>
         </StyledFormDetailContent>
@@ -27,4 +49,11 @@ const StyledFormDetailContent = styled.div`
     gap: 48px;
     width: 100%;
     height: 900px;
+`;
+
+const NavigationBar = styled.div`
+    ${flex({ alignItems: 'center' })}
+    width: 100%;
+    height: 60px;
+    background-color: ${color.white};
 `;
