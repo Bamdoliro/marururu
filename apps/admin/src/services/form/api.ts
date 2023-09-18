719774710cf43af124d3797d4e2dd446d731e327
이명재 <arkk20000@gmail.com>
2023-09-18 23:57:45 +0900

    ADD :: 액셀 내보내기 기능

--- apps/admin/src/services/form/api.ts
+++ apps/admin/src/services/form/api.ts
@@ -1,6 +1,6 @@
 import { maru } from '@/apis/instance/instance';
 import { authorization } from '@/apis/token';
-import { FormListType } from '@/types/form/client';
+import { ExportExcelType, FormListType } from '@/types/form/client';
 import { GetFormListRes } from '@/types/form/remote';
 
 export const getFormList = async (formListType: FormListType) => {
@@ -25,6 +25,42 @@ export const getSecondScoreFormat = async () => {
 
 export const patchSecondScoreFormat = async (formData: FormData) => {
     const { data } = await maru.patch('/form/second-round', formData, authorization.FormData());
+
+    return data;
+};
+
+export const getExportExcel = async (exportExcelType: ExportExcelType) => {
+    if (exportExcelType === '전체 내보내기') {
+        const { data } = await maru.get('/form/xlsx/result', {
+            ...authorization(),
+            responseType: 'blob',
+        });
+
+        return data;
+    }
+
+    if (exportExcelType === '1차 전형 결과') {
+        const { data } = await maru.get('/form/xlsx/first-round', {
+            ...authorization(),
+            responseType: 'blob',
+        });
+
+        return data;
+    }
+
+    if (exportExcelType === '2차 전형 결과') {
+        const { data } = await maru.get('/form/xlsx/second-round', {
+            ...authorization(),
+            responseType: 'blob',
+        });
+
+        return data;
+    }
+
+    const { data } = await maru.get('/form/xlsx/final-passed', {
+        ...authorization(),
+        responseType: 'blob',
+    });
 
     return data;
 };
