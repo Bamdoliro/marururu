17febc2275c1336614f2d09b28cf73c97cbec70d
이명재 <arkk20000@gmail.com>
2023-09-19 23:39:10 +0900

    ADD :: 전형 페이지

--- apps/admin/src/components/form/FormDetailContent/FormDetailContent.tsx
+++ apps/admin/src/components/form/FormDetailContent/FormDetailContent.tsx
@@ -8,6 +8,7 @@ import styled from 'styled-components';
 import FormStatus from './FormStatus/FormStatus';
 import Profile from './Profile/Profile';
 import 보호자정보 from './보호자정보/보호자정보';
+import 전형 from './전형/전형';
 import 지원자정보 from './지원자정보/지원자정보';
 import 출신학교및학력 from './출신학교및학력/출신학교및학력';
 
@@ -46,6 +47,7 @@ const FormDetailContent = ({ id }: Props) => {
                             '지원자 정보': <지원자정보 id={id} />,
                             '보호자 정보': <보호자정보 id={id} />,
                             '출신학교 및 학력': <출신학교및학력 id={id} />,
+                            전형: <전형 id={id} />,
                         }}
                     />
                 </Column>
