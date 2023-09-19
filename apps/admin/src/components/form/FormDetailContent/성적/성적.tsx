bb87d01f53cfdb7abbeb1bc25fea27b903f2340e
이명재 <arkk20000@gmail.com>
2023-09-20 03:43:53 +0900

    ADD :: 봉사 시간 페이지

--- apps/admin/src/components/form/FormDetailContent/성적/성적.tsx
+++ apps/admin/src/components/form/FormDetailContent/성적/성적.tsx
@@ -4,6 +4,7 @@ import { Column, Row } from '@maru/ui';
 import { SwitchCase } from '@toss/react';
 import { useState } from 'react';
 import 교과성적 from './교과성적/교과성적';
+import 봉사시간 from './봉사시간/봉사시간';
 import 출결상황 from './출결상황/출결상황';
 
 interface Props {
@@ -29,6 +30,7 @@ const 성적 = ({ id }: Props) => {
                 caseBy={{
                     '교과 성적': <교과성적 id={id} />,
                     '출결 상황': <출결상황 id={id} />,
+                    '봉사 시간': <봉사시간 id={id} />,
                 }}
             />
         </Row>
