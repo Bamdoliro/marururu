ed6db6c321e7277d62c2b540ebbbc690da09951e
SEOKKAMONI <sj01048260279@gmail.com>
2023-11-24 08:56:55 +0900

    chore(user): formatDate 가독성 향상

--- apps/user/src/utils/formatDate.ts
+++ apps/user/src/utils/formatDate.ts
@@ -1,5 +1,7 @@
 const formatDate = (date: string) => {
-  return date.replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3');
+  const formattedDate = date.replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3');
+
+  return formattedDate;
 };
 
 export default formatDate;
