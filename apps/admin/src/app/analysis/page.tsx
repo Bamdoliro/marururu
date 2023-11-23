95e23f7b8779c218e0681e1bfe58b43edbd98eda
이명재 <arkk20000@gmail.com>
2023-11-23 13:41:43 +0900

    feat(admin): 임시 분석 페이지 제작

---
+++ apps/admin/src/app/analysis/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import AppLayout from '@/layouts/AppLayout';
+import { flex } from '@maru/utils';
+import styled from 'styled-components';
+
+const AnalysisPage = () => {
+  return (
+    <AppLayout>
+      <StyledAnalysisPage>아직 준비되지 않은 페이지입니다 😅</StyledAnalysisPage>
+    </AppLayout>
+  );
+};
+
+export default AnalysisPage;
+
+const StyledAnalysisPage = styled.div`
+  ${flex({ alignItems: 'center', justifyContent: 'center' })}
+  height: 100vh;
+`;
