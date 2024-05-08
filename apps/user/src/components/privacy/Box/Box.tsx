e7f7f8b02272e423566a52751f38c5b778ef77c6
junghongseop <a01082372487@gmail.com>
2024-05-08 17:06:17 +0900

    feat(user) :: 박스 컴포넌트 개발

---
+++ apps/user/src/components/privacy/Box/Box.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { color } from '@maru/design-token';
+import styled from 'styled-components';
+
+interface BoxProps {
+  children: React.ReactNode;
+  backgroundColor?: string;
+}
+
+const Box: React.FC<BoxProps> = ({ children, backgroundColor }) => {
+  return <BoxContainer backgroundColor={backgroundColor}>{children}</BoxContainer>;
+};
+
+export default Box;
+
+const BoxContainer = styled.div<{ backgroundColor?: string }>`
+  width: 82%;
+  padding: 24px 70px;
+  border-radius: 6px;
+  border: 1px solid ${color.gray400};
+  margin: 20px 0;
+  background-color: ${(props) => props.backgroundColor};
+  text-align: left;
+`;
