e74941421ca1ab47ab567895e054719133603f72
oxjadex <oharin765@gmail.com>
2024-05-13 17:55:59 +0900

    fix(admin): 지원자 전형 퍼블리싱

--- apps/admin/src/app/analysis/numberOfApplicants/numberOfApplicants.tsx
+++ apps/admin/src/app/analysis/numberOfApplicants/numberOfApplicants.tsx
@@ -1,19 +1,141 @@
-import { useFormDetailQuery } from '@/services/form/queries';
-import DataBox from '@/components/common/DataBox/DataBox';
-import { FORM_TYPE } from '@/constants/main/constants';
+import { Column, Row, Td, Th } from '@maru/ui';
 
-interface Props {
-  id: number;
-}
-
-const NumberOfApplicants = ({ id }: Props) => {
-  const { data: formDetailData } = useFormDetailQuery(id);
-
+const NumberOfApplicants = () => {
   return (
-    <DataBox
-      label="지원자 수 (경쟁률)"
-      data={formDetailData ? FORM_TYPE[formDetailData.type] : ''}
-    />
+    <Column>
+      <Row>
+        <Th width={300} height={56} borderTopLeftRadius={12}>
+          전형
+        </Th>
+        <Th width={300} height={56}>
+          유형
+        </Th>
+        <Th width={300} height={56}>
+          구분
+        </Th>
+        <Th width={80} height={56} borderTopRightRadius={12}>
+          지원자
+        </Th>
+      </Row>
+      <Row>
+        <Td width={300} height={56}>
+          일반전형
+        </Td>
+        <Td width={300} height={56}>
+          <div></div>
+        </Td>
+        <Td width={300} height={56}>
+          <div></div>
+        </Td>
+        <Td width={80} height={56}>
+          <div>숫자</div>
+        </Td>
+      </Row>
+      <Row>
+        <Td width={300} height={560} borderBottomLeftRadius={12}>
+          특별전형
+        </Td>
+        <Column>
+          <Row>
+            <Td width={300} height={56}>
+              마이스터 인재 전형
+            </Td>
+            <Td width={300} height={56}>
+              <div></div>
+            </Td>
+            <Td width={80} height={56}>
+              <div></div>
+            </Td>
+          </Row>
+          <Row>
+            <Td width={300} height={280}>
+              기호균등 전형
+            </Td>
+            <Column>
+              <Row>
+                <Td width={300} height={56}>
+                  국민기초생활수급자
+                </Td>
+                <Td width={80} height={56}>
+                  <div></div>
+                </Td>
+              </Row>
+              <Row>
+                <Td width={300} height={56}>
+                  차상위계층
+                </Td>
+                <Td width={80} height={56}>
+                  <div></div>
+                </Td>
+              </Row>
+              <Row>
+                <Td width={300} height={56}>
+                  국가보훈대상자 (국가유공자)
+                </Td>
+                <Td width={80} height={56}>
+                  <div></div>
+                </Td>
+              </Row>
+              <Row>
+                <Td width={300} height={56}>
+                  한부모가정보호대상자
+                </Td>
+                <Td width={80} height={56}>
+                  <div></div>
+                </Td>
+              </Row>
+              <Row>
+                <Td width={300} height={56}>
+                  북한이탈주민 또는 그 자녀
+                </Td>
+                <Td width={80} height={56}>
+                  <div></div>
+                </Td>
+              </Row>
+            </Column>
+          </Row>
+          <Row>
+            <Td width={300} height={224}>
+              사회다양성 전형
+            </Td>
+            <Column>
+              <Row>
+                <Td width={300} height={56}>
+                  다문화가정 자녀
+                </Td>
+                <Td width={80} height={56}>
+                  <div></div>
+                </Td>
+              </Row>
+              <Row>
+                <Td width={300} height={56}>
+                  소년 · 소녀가장
+                </Td>
+                <Td width={80} height={56}>
+                  <div></div>
+                </Td>
+              </Row>
+              <Row>
+                <Td width={300} height={56}>
+                  다자녀 가정 자녀
+                </Td>
+                <Td width={80} height={56}>
+                  <div></div>
+                </Td>
+              </Row>
+              <Row>
+                <Td width={300} height={56}>
+                  농어촌지역출신자
+                </Td>
+                <Td width={80} height={56}>
+                  <div></div>
+                </Td>
+              </Row>
+            </Column>
+          </Row>
+        </Column>
+      </Row>
+    </Column>
   );
 };
 
--- apps/admin/src/app/analysis/page.tsx
+++ apps/admin/src/app/analysis/page.tsx
@@ -14,51 +14,40 @@ import GenderRatio from './genderRatio/genderRatio';
 import SchoolOrigin from './schoolOrigin/schoolOrigin';
 import styled from 'styled-components';
 
-interface Props {
-  id: number;
-}
-
 const AnalysisPage = () => {
+  const [currentAnalysisStep, setCurrentAnalysisStep] = useState('지원자 수 (경쟁률)');
+
   return (
     <AppLayout>
       <StyledAnalysisPage>
         <Text fontType="H1">분석</Text>
-        <AnalysisPageContent id={1} />
+        <Row gap={48}>
+          <Column gap={10}>
+            {ANALYSIS_STEP_LIST.map((analysisstep, index) => (
+              <SideMenu
+                key={`analysis-step ${index}`}
+                active={currentAnalysisStep === analysisstep}
+                onClick={() => setCurrentAnalysisStep(analysisstep)}
+              >
+                {analysisstep}
+              </SideMenu>
+            ))}
+          </Column>
+          <SwitchCase
+            value={currentAnalysisStep}
+            caseBy={{
+              '지원자 수 (경쟁률)': <NumberOfApplicants />,
+              '성적 분포': <GradeDistribution />,
+              '지원 전형 비율': <ApplyingType />,
+              '지원자 성비': <GenderRatio />,
+              '출신 학교 현황': <SchoolOrigin />,
+            }}
+          />
+        </Row>
       </StyledAnalysisPage>
     </AppLayout>
   );
 };
-
-const AnalysisPageContent = ({ id }: Props) => {
-  const [currentAnalysisStep, setCurrentAnalysisStep] = useState('지원자 수 (경쟁률)');
-
-  return (
-    <Row gap={48}>
-      <Column gap={10}>
-        {ANALYSIS_STEP_LIST.map((analysisstep, index) => (
-          <SideMenu
-            key={`graduation-step ${index}`}
-            active={currentAnalysisStep === analysisstep}
-            onClick={() => setCurrentAnalysisStep(analysisstep)}
-          >
-            {analysisstep}
-          </SideMenu>
-        ))}
-      </Column>
-      <SwitchCase
-        value={currentAnalysisStep}
-        caseBy={{
-          '지원자 수 (경쟁률)': <NumberOfApplicants id={id} />,
-          '성적 분포': <GradeDistribution />,
-          '지원 전형 비율': <ApplyingType />,
-          '지원자 성비': <GenderRatio />,
-          '출신 학교 현황': <SchoolOrigin />,
-        }}
-      />
-    </Row>
-  );
-};
-
 export default AnalysisPage;
 
 const StyledAnalysisPage = styled.div`
