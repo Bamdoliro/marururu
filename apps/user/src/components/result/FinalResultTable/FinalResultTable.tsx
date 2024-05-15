8f3e776eac26246880f73bbe5d3d9923f50120d6
junghongseop <a01082372487@gmail.com>
2024-05-15 23:02:20 +0900

    fix(user) : 결과페이지 퍼블리싱 수정

--- apps/user/src/components/result/FinalResultTable/FinalResultTable.tsx
+++ apps/user/src/components/result/FinalResultTable/FinalResultTable.tsx
@@ -2,25 +2,21 @@ import { useFinalResultQuery } from '@/services/result/queries';
 import { Column } from '@maru/ui';
 import { flex } from '@maru/utils';
 import styled from 'styled-components';
-import ResultTableFooter from '../ResultTableFooter/ResultTableFooter';
-import ResultTableHeader from '../ResultTableHeader/ResultTableHeader';
-import ResultTableItem from '../ResultTableItem/ResultTableItem';
+import ResultTableFooter from '../ResultTableFooter/final/ResultTableFooter';
+import FinalResultTableItem from '../FinalResultTableItem/FinalResultTableItem';
 
 const FinalResultTable = () => {
   const { data: finalResultData } = useFinalResultQuery();
 
   return finalResultData ? (
     <StyledFinalResultTable isPassed={finalResultData.passed}>
-      <Column gap={12} width={816}>
-        <ResultTableHeader />
-        <ResultTableItem
-          id={finalResultData.id}
-          name={finalResultData.name}
+      <Column gap={20} width={816}>
+        <FinalResultTableItem
           type={finalResultData.type}
           isPassed={finalResultData.passed}
         />
+        <ResultTableFooter option="FINAL" isPassed={finalResultData.passed} />
       </Column>
-      <ResultTableFooter option="FINAL" isPassed={finalResultData.passed} />
     </StyledFinalResultTable>
   ) : null;
 };
@@ -29,5 +25,5 @@ export default FinalResultTable;
 
 const StyledFinalResultTable = styled.div<{ isPassed: boolean }>`
   ${flex({ flexDirection: 'column', alignItems: 'center' })};
-  gap: ${(props) => (props.isPassed ? '64px' : '120px')};
+  gap: ${(props) => (props.isPassed ? '1px' : '120px')};
 `;
---
+++ apps/user/src/components/result/FirstResultTableItem/FirstResultTableItem.tsx
@@ -0,0 +1,34 @@
+import { color } from '@maru/design-token';
+import { Column, Text } from '@maru/ui';
+import styled from 'styled-components';
+
+interface Props {
+  type: string;
+  isPassed: boolean;
+}
+
+const FirstResultTableItem = ({ type, isPassed }: Props) => {
+  return (
+    <StyledResultTableItem>
+      <Column alignItems="center" gap={10}>
+        <Text fontType="p1" color={color.gray600}>
+          {type}
+        </Text>
+        <Text fontType="H2" color={color.maruDefault}>
+          {isPassed ? '합격을 축하합니다.' : '1차 합격자 명단에 없습니다.'}
+        </Text>
+      </Column>
+    </StyledResultTableItem>
+  );
+};
+
+export default FirstResultTableItem;
+
+const StyledResultTableItem = styled.div`
+  align-items: center;
+  justify-content: space-between;
+  display: inline-block;
+  width: 100%;
+  height: 64px;
+  background-color: ${color.white};
+`;
