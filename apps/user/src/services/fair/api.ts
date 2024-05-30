00ed4d6278e69efe0a1239e9fe8551ae31ff2d64
junghongseop <a01082372487@gmail.com>
2024-05-31 00:29:23 +0900

    fix(user): api url 오타 수정

--- apps/user/src/services/fair/api.ts
+++ apps/user/src/services/fair/api.ts
@@ -2,6 +2,6 @@ import { maru } from '@/apis/instance/instance';
 import type { GetFairListRes } from '@/types/fair/remote';
 
 export const getFairList = async (fairType: string) => {
-  const { data } = await maru.get<GetFairListRes>(`/fair?fairType=${fairType}`);
+  const { data } = await maru.get<GetFairListRes>(`/fair?type=${fairType}`);
   return data;
 };
