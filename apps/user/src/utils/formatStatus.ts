ef72556f38af9d6a18e4355f083549ea0554764f
junghongseop <a01082372487@gmail.com>
2024-05-31 11:34:12 +0900

    fix(user): 상태 문구 수정

--- apps/user/src/utils/formatStatus.ts
+++ apps/user/src/utils/formatStatus.ts
@@ -1,9 +1,9 @@
 const statusMap: { [key: string]: string } = {
-  APPLICATION_ENDED: '신청 종료됨',
-  CLOSED: '종료됨',
-  APPLICATION_IN_PROGRESS: '신청 진행 중',
-  APPLICATION_NOT_STARTED: '신청 시작 전',
-  APPLICATION_EARLY_CLOSED: '신청 조기 종료됨',
+  APPLICATION_ENDED: '신청 마감',
+  CLOSED: '종료',
+  APPLICATION_IN_PROGRESS: '진행 중',
+  APPLICATION_NOT_STARTED: '준비 중',
+  APPLICATION_EARLY_CLOSED: '조기 마감',
 };
 
 export const formatStatus = (status: string) => {
