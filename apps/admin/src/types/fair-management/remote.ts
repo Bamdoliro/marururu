de427f465b763c13d29f04fc9f5ac1ffe043acc8
junghongseop <a01082372487@gmail.com>
2024-06-12 04:55:02 +0900

    feat(admin): 입학설명회 일정 생성 axios 연결

--- apps/admin/src/types/fair-management/remote.ts
+++ apps/admin/src/types/fair-management/remote.ts
@@ -3,3 +3,12 @@ import type { Fair } from './client';
 export interface GetFairListRes {
   dataList: Fair[];
 }
+
+export interface PostFairReq {
+  start: string;
+  capacity: number;
+  place: string;
+  type: string;
+  applicationStartDate: string;
+  applicationEndDate: string;
+}
