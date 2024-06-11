b35be87476b9bb958babf033a022162534c95edc
junghongseop <a01082372487@gmail.com>
2024-06-11 19:23:24 +0900

    ADD(admin): 입학 설명회 application box에서 format이 필요한 부분들을 위한 파일 추가

--- apps/admin/src/utils/index.ts
+++ apps/admin/src/utils/index.ts
@@ -1,1 +1,4 @@
 export { default as resizeTextarea } from './functions/resizeTextarea';
+export { default as formatApplicationDate } from './functions/formatApplicationDate';
+export { default as formatStartDate } from './functions/formatStartDate';
+export { formatStatus } from './functions/formatStatus';
