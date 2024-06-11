8075350a02ee2b7e6b4cc6fcdb3aba4599865a56
junghongseop <a01082372487@gmail.com>
2024-06-11 15:37:01 +0900

    feat(admin): 입학 설명회 일정 조회 axios 추가

---
+++ apps/admin/src/services/fair-management/api.ts
@@ -0,0 +1,7 @@
+import { maru } from '@/apis/instance/instance';
+import type { GetFairListRes } from '@/types/fair-management/remote';
+
+export const getFairList = async (fairType: string) => {
+  const { data } = await maru.get<GetFairListRes>(`/fair?type=${fairType}`);
+  return data;
+};
