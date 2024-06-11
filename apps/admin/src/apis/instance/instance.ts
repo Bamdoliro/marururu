39dd4188e979af29213484148228c8bdf6ed558e
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2024-06-12 08:23:26 +0900

    chore(admin): baseURL 복구

--- apps/admin/src/apis/instance/instance.ts
+++ apps/admin/src/apis/instance/instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const maru = axios.create({
-  baseURL: 'https://maru-base.com',
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   timeout: 15000,
   headers: {
     'Content-Type': 'application/json',
