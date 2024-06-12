a7b2290ccb06367a9f246fe1b24788653f1bbbd2
junghongseop <a01082372487@gmail.com>
2024-06-13 00:37:06 +0900

    ADD: Input수정 및 format 파일 추가

--- apps/admin/src/utils/index.ts
+++ apps/admin/src/utils/index.ts
@@ -2,3 +2,5 @@ export { default as resizeTextarea } from './functions/resizeTextarea';
 export { default as formatApplicationDate } from './functions/formatApplicationDate';
 export { default as formatStartDate } from './functions/formatStartDate';
 export { formatStatus } from './functions/formatStatus';
+export { default as formatDate } from './functions/formatDate';
+export { default as formatTime } from './functions/formatTime';
