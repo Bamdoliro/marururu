cbfa35d59d4ad1f1ce74f9f2f0c35dca075bb7c3
junghongseop <a01082372487@gmail.com>
2024-06-18 02:06:59 +0900

    fix(user): 봉사시간 및 출결 상황 마감 기한 수정

--- apps/user/src/components/form/Calculators/AttendanceCalculator/AttendanceCalculator.tsx
+++ apps/user/src/components/form/Calculators/AttendanceCalculator/AttendanceCalculator.tsx
@@ -12,7 +12,7 @@ const AttendanceCalculator = () => {
   return (
     <StyledAttendanceCalculator>
       <Text fontType="p3" color={color.red}>
-        *2023.09.27까지의 출결상황을 기재해주세요. 졸업생은 졸업일 기준으로 기재해주세요.
+        *2024.09.30까지의 출결상황을 기재해주세요. 졸업생은 졸업일 기준으로 기재해주세요.
       </Text>
       <Column>
         <Row>
