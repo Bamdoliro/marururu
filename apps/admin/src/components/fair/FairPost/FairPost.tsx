cd9248cd75b8261d95d246aefa3fe5f2409a58d5
junghongseop <a01082372487@gmail.com>
2024-06-18 18:04:08 +0900

    feat(admin): 일정 생성 기능 개발

--- apps/admin/src/components/fair/FairPost/FairPost.tsx
+++ apps/admin/src/components/fair/FairPost/FairPost.tsx
@@ -1,9 +1,11 @@
 import { color } from '@maru/design-token';
-import { flex } from '@maru/utils';
-import { useState } from 'react';
+import { flex, formatTime } from '@maru/utils';
+import type { ChangeEvent } from 'react';
+import { useEffect, useState } from 'react';
 import { styled } from 'styled-components';
 import { useFairPostAction } from './FairPost.hooks';
 import { Button, Column, Input, RadioGroup, Row } from '@maru/ui';
+import { formatDate } from '@/utils';
 
 const FairPost = () => {
   const [fairData, setFairData] = useState({
@@ -20,6 +22,49 @@ const FairPost = () => {
 
   const { handleFairPostButtonClick } = useFairPostAction(fairData);
 
+  useEffect(() => {
+    if (fairData.startDate && fairData.startTime) {
+      const combinedStart = `${fairData.startDate}T${fairData.startTime}:00`;
+      setFairData((prevData) => ({
+        ...prevData,
+        start: combinedStart,
+      }));
+    }
+  }, [fairData.startDate, fairData.startTime]);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    if (
+      name === 'startDate' ||
+      name === 'applicationStartDate' ||
+      name === 'applicationEndDate'
+    ) {
+      setFairData((prevData) => ({
+        ...prevData,
+        [name]: formatDate(value),
+      }));
+    } else if (name === 'startHour') {
+      setFairData((prevData) => ({
+        ...prevData,
+        [name]: formatTime(Number(value)),
+      }));
+    } else {
+      setFairData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+    }
+  };
+
+  const handleRadioChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFairData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
   return (
     <StyledPostFairBox>
       <Column gap={30} alignItems="center" width={360}>
@@ -31,7 +76,7 @@ const FairPost = () => {
               name="applicationUrl"
               value={fairData.applicationUrl}
               placeholder="폼의 링크를 삽입해주세요."
-              // onChange={handleInputChange}
+              onChange={handleInputChange}
             />
             <RadioGroup
               label="대상 선택"
@@ -41,7 +86,7 @@ const FairPost = () => {
                 { value: 'TEACHER', label: '교사' },
               ]}
               value={fairData.type}
-              // onChange={handleRadioChange}
+              onChange={handleRadioChange}
             />
             <Input
               label="장소"
@@ -49,7 +94,7 @@ const FairPost = () => {
               name="place"
               placeholder="장소를 입력해주세요."
               value={fairData.place}
-              // onChange={handleInputChange}
+              onChange={handleInputChange}
             />
             <Input
               label="입학설명회 날짜 (8자리)"
@@ -57,7 +102,7 @@ const FairPost = () => {
               name="startDate"
               placeholder="날짜를 입력해주세요."
               value={fairData.startDate}
-              // onChange={handleInputChange}
+              onChange={handleInputChange}
             />
             <Input
               label="시간 (4자리)"
@@ -65,7 +110,7 @@ const FairPost = () => {
               name="startHour"
               placeholder="시간을 입력해주세요."
               value={fairData.startTime}
-              // onChange={handleInputChange}
+              onChange={handleInputChange}
             />
             <Row gap={5} alignItems="center">
               <Input
@@ -74,7 +119,7 @@ const FairPost = () => {
                 name="applicationStartDate"
                 placeholder="시작일"
                 value={fairData.applicationStartDate}
-                // onChange={handleInputChange}
+                onChange={handleInputChange}
               />
               <Input
                 label="ㅤ"
@@ -82,7 +127,7 @@ const FairPost = () => {
                 name="applicationEndDate"
                 placeholder="마감일"
                 value={fairData.applicationEndDate}
-                // onChange={handleInputChange}
+                onChange={handleInputChange}
               />
             </Row>
           </Column>
