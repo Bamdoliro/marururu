5e99b845af4897f0e9c9b3bad4fffea1e5f1ef84
junghongseop <a01082372487@gmail.com>
2024-06-18 17:56:38 +0900

    feat(admin): 입학 설명회 일정 생성 페이지 퍼블리싱

--- apps/admin/src/app/fair/post/page.tsx
+++ apps/admin/src/app/fair/post/page.tsx
@@ -3,13 +3,20 @@
 import AppLayout from '@/layouts/AppLayout';
 import { styled } from 'styled-components';
 import { flex } from '@maru/utils';
-import { Text } from '@maru/ui';
+import { Loader, Text } from '@maru/ui';
+import { Suspense } from 'react';
+import { FairPost } from '@/components/fair';
 
 const FairPostPage = () => {
   return (
     <AppLayout>
       <StyledFairPost>
         <Text fontType="H1">입학전형 설명회 생성</Text>
+        <Center>
+          <Suspense fallback={<Loader />}>
+            <FairPost />
+          </Suspense>
+        </Center>
       </StyledFairPost>
     </AppLayout>
   );
@@ -20,8 +27,12 @@ export default FairPostPage;
 const StyledFairPost = styled.div`
   position: relative;
   ${flex({ flexDirection: 'column' })}
-  gap: 24px;
+  gap: 15px;
   width: 100%;
   min-height: 100vh;
   padding: 64px 75px;
+`;
+
+const Center = styled.div`
+  ${flex({ justifyContent: 'center', alignItems: 'center' })}
 `;
--- apps/admin/src/components/fair/index.ts
+++ apps/admin/src/components/fair/index.ts
@@ -1,1 +1,2 @@
 export { default as Header } from './Header/header';
+export { default as FairPost } from './FairPost/FairPost';
