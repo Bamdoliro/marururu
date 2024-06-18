f207ccb609e264dede78b74e731b613bf057542c
junghongseop <a01082372487@gmail.com>
2024-06-18 12:22:49 +0900

    feat(admin): 입학 설명회 관리 페이지 퍼블리싱

---
+++ apps/admin/src/app/fair/page.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { styled } from 'styled-components';
+import { flex } from '@maru/utils';
+import { Loader, Text } from '@maru/ui';
+import AppLayout from '@/layouts/AppLayout';
+import { Header } from '@/components/fair';
+import { Suspense, useEffect, useState } from 'react';
+
+const FairPage = () => {
+  const [status, setStatus] = useState('진행 중인 신청');
+
+  useEffect(() => {
+    const savedStatus = localStorage.getItem('selectedTab');
+    if (savedStatus) {
+      setStatus(savedStatus);
+    }
+  }, []);
+
+  const handleTabClick = (name: string) => {
+    setStatus(name);
+    localStorage.setItem('selectedTab', name);
+  };
+
+  const renderContent = () => {
+    if (status === '진행 중인 신청') {
+      return <>진행 중인 신청</>;
+    } else if (status === '마감된 신청') {
+      return <>마감된 신청</>;
+    }
+  };
+
+  return (
+    <AppLayout>
+      <StyledFairPage>
+        <Text fontType="H1">입학전형 설명회 관리</Text>
+        <Header selectedTab={status} handleTabClick={handleTabClick} />
+        <Suspense fallback={<Loader />}>{renderContent()}</Suspense>
+      </StyledFairPage>
+    </AppLayout>
+  );
+};
+
+export default FairPage;
+
+const StyledFairPage = styled.div`
+  ${flex({ flexDirection: 'column' })}
+  gap: 40px;
+  width: 100%;
+  min-height: 100vh;
+  padding: 64px 75px;
+`;
