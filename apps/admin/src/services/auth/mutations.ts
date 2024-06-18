2cd90e8c01e72bea88329abe220a5434860640a2
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2024-06-18 19:50:39 +0900

    feat(admin): 로그아웃 시 로그인 페이지로 변경

--- apps/admin/src/services/auth/mutations.ts
+++ apps/admin/src/services/auth/mutations.ts
@@ -26,11 +26,12 @@ export const useLoginAdminMutation = ({ phoneNumber, password }: PostLoginAuthRe
 };
 
 export const useLogoutAdminMutation = () => {
+  const router = useRouter();
   const { mutate: logoutAdminMutate, ...restMutation } = useMutation({
     mutationFn: deleteLogoutAdmin,
     onSuccess: () => {
       localStorage.clear();
-      window.location.href = ROUTES.MAIN;
+      router.replace(ROUTES.LOGIN);
     },
     onError: () => localStorage.clear(),
   });
