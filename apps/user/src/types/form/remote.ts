e75b2bc2779d1eb769d08811e4f08761a76d26f4
junghongseop <a01082372487@gmail.com>
2024-06-19 15:24:58 +0900

    fix(user): 불필요 인터페이스 삭제

--- apps/user/src/types/form/remote.ts
+++ apps/user/src/types/form/remote.ts
@@ -1,14 +1,4 @@
-import type {
-  Attendance,
-  Education,
-  Form,
-  FormStatus,
-  FormType,
-  Parent,
-  School,
-  StudentSubject,
-  User,
-} from './client';
+import type { Form, FormStatus, School } from './client';
 
 export interface GetSaveFormRes {
   data: Form;
@@ -20,25 +10,4 @@ export interface GetSchoolListRes {
 
 export interface GetFormStatusRes {
   data: FormStatus;
-}
-
-export interface PutFormReq {
-  applicant: User;
-  parent: Parent;
-  education: Education;
-  grade: {
-    subjectList: StudentSubject[];
-    attendance1: Attendance;
-    attendance2: Attendance;
-    attendance3: Attendance;
-    volunteerTime1: number;
-    volunteerTime2: number;
-    volunteerTime3: number;
-    certificateList: string[];
-  };
-  document: {
-    coverLetter: string;
-    statementOfPurpose: string;
-  };
-  type: FormType;
 }
