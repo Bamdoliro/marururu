ee81a9ba55f4b02e19a3db15734d856984fafc67
oxjadex <oharin765@gmail.com>
2024-07-06 14:01:38 +0900

    feat(user): /form 막기

--- apps/user/src/hocs/withAuth.tsx
+++ apps/user/src/hocs/withAuth.tsx
@@ -18,7 +18,7 @@ const withAuth = (Component: React.ComponentType) => {
     const router = useRouter();
     const [isMounted, setIsMounted] = useState(false);
     const hasAccessToken = Boolean(Storage.getItem(TOKEN.ACCESS));
-    const LOGIN_TYPE = data?.data.authority;
+    const loginType = data?.data.authority;
     const overlay = useOverlay();
     const now = dayjs();
 
@@ -27,27 +27,29 @@ const withAuth = (Component: React.ComponentType) => {
         setIsMounted(true);
       }
     }, []);
-
     useEffect(() => {
-      if (isMounted && LOGIN_TYPE === 'ADMIN') {
-        alert('권한이 없어 원서 작성이 불가합니다.');
-        router.replace(ROUTES.LOGIN);
-        localStorage.clear();
-      } else if (isMounted && !hasAccessToken) {
-        overlay.open(({ isOpen, close }) => (
-          <NeedLoginModal isOpen={isOpen} onClose={close} />
-        ));
-        return;
-      } else if (!now.isBetween(제출_시작_날짜, 제출_마감_날짜)) {
-        overlay.open(({ isOpen, close }) => (
-          <GuardFormModal isOpen={isOpen} onClose={close} />
-        ));
-        return;
+      if (isMounted) {
+        if (!now.isBetween(제출_시작_날짜, 제출_마감_날짜)) {
+          overlay.open(({ isOpen, close }) => (
+            <GuardFormModal isOpen={isOpen} onClose={close} />
+          ));
+          return;
+        } else if (loginType === 'ADMIN') {
+          alert('권한이 없어 원서 작성이 불가합니다.');
+          router.replace(ROUTES.MAIN);
+        } else if (!hasAccessToken) {
+          overlay.open(({ isOpen, close }) => (
+            <NeedLoginModal isOpen={isOpen} onClose={close} />
+          ));
+        } else if (loginType === undefined || loginType === null) {
+          alert('로그인 정보가 없습니다.');
+          router.replace(ROUTES.MAIN);
+          localStorage.clear();
+        }
       }
-    }, [isMounted, hasAccessToken, router, now, LOGIN_TYPE, overlay]);
+    }, [isMounted, hasAccessToken, router, now, loginType, overlay]);
     return <Component />;
   };
-
   return WrappedComponent;
 };
 
