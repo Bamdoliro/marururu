db03159d86f3bac8fefb3da23c0ebabc30f67a8d
정홍섭 <126876363+junghongseop@users.noreply.github.com>
2024-07-09 16:34:13 +0900

    fix(user): 교무부 발표 중 일정 수정

--- apps/user/src/components/main/ScheduleBox/ScheduleBox.tsx
+++ apps/user/src/components/main/ScheduleBox/ScheduleBox.tsx
@@ -17,28 +17,28 @@ import ScheduleItem from './ScheduleBoxItem/ScheduleBoxItem';
 const SCHEDULE_DATA = [
   {
     id: 0,
-    date: '2024년 07월 09일 ~ 07월09일 16:28',
+    date: '2024년 07월 09일 ~ 07월09일 16:38',
     plan: '1차 원서접수',
     startTime: 제출_시작_날짜,
     endTime: 제출_마감_날짜,
   },
   {
     id: 1,
-    date: '2024년 07월 09일 16:30',
+    date: '2024년 07월 09일 16:43',
     plan: '1차 합격자 발표',
     startTime: 일차_합격_발표,
     endTime: 이차_전형_시작,
   },
   {
     id: 2,
-    date: '2024년 07월 09일 16:32 ~ 16:34',
+    date: '2024년 07월 09일 16:32 ~ 16:48',
     plan: '2차 전형',
     startTime: 이차_전형_시작,
     endTime: 이차_전형_끝,
   },
   {
     id: 3,
-    date: '2024년 07월 09일 16:36',
+    date: '2024년 07월 09일 16:53',
     plan: '최종 합격자 발표',
     startTime: 최종_합격_발표,
     endTime: 입학_등록_기간,
