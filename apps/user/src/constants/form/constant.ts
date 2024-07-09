80086cac1d204c1eb661501c70019ab8f250333c
junghongseop <a01082372487@gmail.com>
2024-07-09 17:11:16 +0900

    fix(user): 개발을 위해 다시 일정 수정

--- apps/user/src/constants/form/constant.ts
+++ apps/user/src/constants/form/constant.ts
@@ -2,14 +2,14 @@ import dayjs from 'dayjs';
 import 'dayjs/locale/ko';
 dayjs.locale('ko');
 
-export const 제출_시작_날짜 = dayjs('2024-07-09T00:00:00+09:00');
-export const 제출_마감_날짜 = dayjs('2024-07-09T16:38:00+09:00');
-export const 일차_합격_발표 = dayjs('2024-07-09T16:43:00+09:00');
-export const 이차_전형_시작 = dayjs('2024-07-09T16:48:30+09:00');
-export const 이차_전형_끝 = dayjs('2024-07-09T16:53:00+09:00');
-export const 최종_합격_발표 = dayjs('2024-07-09T16:58:00+09:00');
-export const 입학_등록_기간 = dayjs('2024-07-10T00:00:00+09:00');
-export const 입학_등록_기간_마감 = dayjs('2024-07-10T00:00:00+09:00');
+export const 제출_시작_날짜 = dayjs('2024-07-10T00:00:00+09:00');
+export const 제출_마감_날짜 = dayjs('2024-07-11T17:00:00+09:00');
+export const 일차_합격_발표 = dayjs('2024-07-12T15:00:00+09:00');
+export const 이차_전형_시작 = dayjs('2024-07-13T13:00:00+09:00');
+export const 이차_전형_끝 = dayjs('2024-07-13T16:00:00+09:00');
+export const 최종_합격_발표 = dayjs('2024-07-14T15:00:00+09:00');
+export const 입학_등록_기간 = dayjs('2024-07-15T00:00:00+09:00');
+export const 입학_등록_기간_마감 = dayjs('2024-07-16T00:00:00+09:00');
 
 export const REGULAR_TYPE_DEFAULT_SCORE = 80;
 export const SPECIAL_TYPE_DEFAULT_SCORE = 48;
