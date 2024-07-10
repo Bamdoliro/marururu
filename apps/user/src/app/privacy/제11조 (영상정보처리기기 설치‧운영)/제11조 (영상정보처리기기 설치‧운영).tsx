70114a39b0d42d8d35a325099f99514cb84d04a7
junghongseop <a01082372487@gmail.com>
2024-07-10 23:53:27 +0900

    fix(user): 개인정보처리 방침 11조 수정

--- apps/user/src/app/privacy/제11조 (영상정보처리기기 설치‧운영)/제11조 (영상정보처리기기 설치‧운영).tsx
+++ apps/user/src/app/privacy/제11조 (영상정보처리기기 설치‧운영)/제11조 (영상정보처리기기 설치‧운영).tsx
@@ -1,12 +1,16 @@
-/* eslint-disable no-irregular-whitespace */
 import { Text, Row, Column } from '@maru/ui';
 import { color } from '@maru/design-token';
 import { IconTerms } from '@maru/icon';
 import {
   CameraLocation,
-  CameraResponsibilityTeacher,
-  CameraTime,
+  DeviceType,
+  FilmingDay,
+  InformationLocation,
+  ManagerTable,
+  OpenRequestBox,
+  ProcessingRestrictionsBox,
 } from '@/components/privacy';
+import { styled } from 'styled-components';
 
 const 제11조 = () => {
   return (
@@ -18,67 +22,113 @@ const 제11조 = () => {
             제11조 (영상정보처리기기 설치‧운영)
           </Text>
         </Row>
-        <Text fontType="p3" color={color.gray900}>
-          ① 우리학교는 초·중등교육법 제30조의8 제2항 및 개인정보 보호법 제25조제1항에 따라
-          다음과 같은 목적으로 영상정보처리기기를 설치·운영합니다.
+        <TextBlock fontType="p3">
+          ① 영상정보처리기기 설치 목적
           <br />
-          &nbsp; • 학교폭력 예방, 범죄 예방(초·중등교육법 제30조제8항)
+          <Indent>
+            ∙ 교사 내∙외부에 CCTV를 설치하여 학교 및 기숙사 내 보안 및 화재∙도난방지,
+            생활지도 등에 효율적인 대처를 목적으로 함.
+            <br />
+            ∙ 사감실, 당직실에서 CCTV를 통하여 교사(校舍) 밖을 관찰하고 녹화할 수 있어
+            숙직자 업무 경감은 물론 야간 순찰에서 오는 위험을 사전에 차단할 목적임.
+            <br />
+            ∙ 따라서 본교는 개인정보보호법 제 25조 제1항에 따라 다음과 같은 목적으로
+            CCTV를 설치·운영함.
+            <br />
+            <DoubleIndent>
+              a. 시설물 안전 및 화재 예방
+              <br />
+              b. 각종 안전사고 예방
+              <br />
+              c. 학교폭력 예방
+              <br />
+              d. 물품 도난 및 파손 방지
+            </DoubleIndent>
+          </Indent>
+          ② 영상정보처리기기시설 설치‧운영 담당부서, 책임관, 연락처
           <br />
-          &nbsp; • 시설 안전 및 화재 예방, 차량도난 및 파손방지(주차장에 설치하는 경우)
-          <br />② 설치 대수, 설치 위치, 촬영 범위는 다음과 같습니다.
-        </Text>
+          <Indent>
+            ∙ 담당부서: 학생자치부
+            <br />
+            ∙ 책임관: 신용 (051-970-1720)
+            <br />
+            ∙ 운영담당자: 김규봉 (051-970-1771)
+            <br />∙ 실시간 모니터링 전담자 (복수가능)
+          </Indent>
+        </TextBlock>
+        <ManagerTable />
+        <TextBlock fontType="p3">
+          ③ 설치‧운영되는 영상정보처리기기 카메라 대수, 위치, 성능 및 촬영범위
+        </TextBlock>
         <CameraLocation />
-        <Text fontType="p3" color={color.gray900}>
-          ③ 정보주체의 영상정보를 보호하고 개인영상정보와 관련한 불만을 처리하기 위하여
-          아래와 같이 관리책임자와 접근권한자를 두고 있습니다.
-        </Text>
-        <CameraResponsibilityTeacher />
-        <Text fontType="p3" color={color.gray900}>
-          ④ 영상정보 촬영시간, 보관기간, 보관장소는 다음과 같습니다.
-        </Text>
-        <CameraTime />
-        <Text fontType="p3" color={color.gray900}>
-          ⑤ 개인영상정보의 목적 외 이용, 제3자 제공, 파기, 열람 등 요구에 관한 사항을
-          기록·관리하고 보관기간 만료 시 복원이 불가능한 방법으로 영구 삭제(출력물의 경우
-          파<br /> &nbsp;&nbsp;&nbsp;&nbsp;쇄 또는 소각)합니다.
+        <TextBlock fontType="p3">
+          ④ 제7조 제1항에 따라 설치되는 안내판의 규격 및 부착장소
           <br />
-          ⑥ 
-          <Text fontType="p3" color={color.maruDefault}>
-            (업체와 위탁계약을 수립한 경우에만)
-          </Text>
-        </Text>
-        <Text fontType="p3" color={color.gray900}>
-           우리학교는 영상정보처리기기 설치 및 관리 등을 위탁하고 있으며, 관계 법령에 따라
-          위탁계약 시 개인정보가 안전하게 관리될
+          <Indent>
+            ∙ 안내판 규격: 가로 35cm X 세로 25cm
+            <br />∙ 부착장소
+          </Indent>
+        </TextBlock>
+        <InformationLocation />
+        <TextBlock fontType="p3">
+          ⑤ 정보주체의 권리행사 및 불복수단에 관한 내용, 절차 및 방법
           <br />
-          &nbsp;&nbsp;&nbsp;&nbsp;수 있도록 필요한 사항을 규정하고 있습니다. 수탁업체는
-          처리방침 제3조에서 확인하실 수 있습니다.
-          <br />⑦ 개인영상정보의 확인 방법 및 장소는 다음과 같습니다.
+          <Indent>∙ 학교 내 영상정보처리기기 설치, 운영 매뉴얼 제17조 따름</Indent>
+        </TextBlock>
+        <OpenRequestBox />
+        <TextBlock fontType="p3">
+          ⑥ 영상정보처리기기 촬영시간, 영상정보의 보유기간, 영상정보의 보관∙관리∙삭제
+          방법, 영상정보의 보관 장소
           <br />
-          &nbsp;&nbsp;1. 확인 방법 : 개인영상정보 보호담당자에게 개인영상정보 열람존재확인
-          청구서를 제출하시면 내부 심의 후 공개로 결정될 시 확인하실 수 있습니다.
+          <Indent>
+            ∙ 영상정보처리기기 촬영시간 24시간
+            <br />
+            ∙ 영상정보의 보유기간 촬영일로부터 30일
+            <br />
+            ∙ 영상정보의 보관‧관리‧삭제의 방법 시스템의 저장 용량에 따라 30일의 보유 기간
+            후에는 순차적으로 자동 삭제됨
+            <br />∙ 영상정보의 보관 장소 별도의 저장매체 없이 녹화기 자체에 보관
+          </Indent>
+        </TextBlock>
+        <TextBlock fontType="p3">
+          ⑦ 영상정보처리기기에 의하여 전송되는 영상정보가 실제로 열람‧재생되는 장소 및
+          출입통제 현황
           <br />
-          &nbsp;&nbsp;2. 확인 장소 :{' '}
-          <Text fontType="p3" color={color.maruDefault}>
-            경비실, 전산실
-          </Text>
-          <br />⑧ 개인영상정보의 열람 또는 존재 확인 및 삭제를 원할 경우, 개인영상정보
-          담당자에게 요구할 수 있습니다. 단, 정보주체 자신이 촬영된 개인영상정보이거나
-          명백히 정<br />
-          &nbsp;&nbsp;&nbsp;&nbsp;보주체의 급박한 생명, 신체, 재산의 이익을 위해 필요한
-          경우에 한정됩니다. 개인영상정보의 열람 또는 존재확인·삭제 요구 시, 지체없이
-          필요한 조치를 하겠습니다.
-          <br />⑨ 처리되는 영상정보는 암호화 조치 등을 통하여 안전하게 관리되고 있으며,
-          관리적 대책으로 접근 권한 차등부여, 위·변조 방지를 위한 개인영상정보의 생성
-          일시, 열람
+          <Indent>
+            ∙ 열람·재생되는 장소 융합관 층 전산실 학생지도실 본관 보안실 사감실 기숙사
+            <br />∙ 기숙사 출입통제 현황
+          </Indent>
+        </TextBlock>
+        <DeviceType />
+        <TextBlock fontType="p3">
+          ⑧ 영상정보처리기기 실시간 모니터링 관련 사항(시간대별 전담 인력 지정 및 운용,
+          통합관제센터 연계 포함)
           <br />
-          &nbsp;&nbsp;&nbsp;&nbsp;시 열람 목적·열람자·열람 일시 등을 기록·관리하고
-          있습니다. 이 외에도 개인영상정보의 안전한 물리적 보관을 위해 잠금장치를
-          설치·운영하고 있습니다.
-        </Text>
+          <Indent>∙ 시간대별 실시간 모니터링 담당자 지정 및 운용</Indent>
+        </TextBlock>
+        <FilmingDay />
+        <TextBlock fontType="p3">
+          ⑨ 녹화된 영상정보를 제3자에게 제공하거나 열람‧재생하도록 할 수 있는 사유 와 그
+          절차 및 방법
+          <br />
+          <Indent>∙ 학교 내 영상정보처리기기 설치∙운영 매뉴얼 제15조에 따름</Indent>
+        </TextBlock>
+        <ProcessingRestrictionsBox />
       </Column>
     </>
   );
 };
 
 export default 제11조;
+
+const TextBlock = styled(Text).attrs({ fontType: 'p3', color: color.gray900 })`
+  line-height: 1.5;
+`;
+
+const Indent = styled.div`
+  margin-left: 10px;
+`;
+
+const DoubleIndent = styled.div`
+  margin-left: 20px;
+`;
