b54d9dd9f5ff88ed9d751665935b33c817496e74
junghongseop <a01082372487@gmail.com>
2024-07-13 10:13:06 +0900

    fix(user): 고입검정일 경우 졸업구분, 합격연도를 제외하고는 null이여도 되도록 수정

--- apps/user/src/app/form/출신학교및학력/출신학교및학력.hooks.ts
+++ apps/user/src/app/form/출신학교및학력/출신학교및학력.hooks.ts
@@ -1,8 +1,6 @@
-import { useEffect } from 'react';
 import { useSaveFormMutation } from '@/services/form/mutations';
 import { useFormValueStore, useSetFormStepStore, useSetFormStore } from '@/store';
 import type { ChangeEventHandler } from 'react';
-import { useUser } from '@/hooks';
 
 export const useCTAButton = () => {
   const form = useFormValueStore();
@@ -23,54 +21,6 @@ export const useCTAButton = () => {
 
 export const useInput = () => {
   const setForm = useSetFormStore();
-  const user = useUser();
-
-  useEffect(() => {
-    if (user.userData.name && user.userData.phoneNumber) {
-      setForm((prev) => ({
-        ...prev,
-        applicant: {
-          ...prev.applicant,
-          name: user.userData.name,
-          phoneNumber: user.userData.phoneNumber,
-        },
-      }));
-    }
-  }, [setForm, user.userData]);
-
-  const form = useFormValueStore();
-
-  useEffect(() => {
-    if (form.education.graduationType === 'QUALIFICATION_EXAMINATION') {
-      setForm((prev) => ({
-        ...prev,
-        education: {
-          ...prev.education,
-          schoolName: '(고입검정)',
-          schoolAddress: '-',
-          schoolLocation: '-',
-          schoolCode: '-',
-          teacherPhoneNumber: '-',
-          teacherName: '-',
-          teacherMobilePhoneNumber: '-',
-        },
-      }));
-    } else {
-      setForm((prev) => ({
-        ...prev,
-        education: {
-          ...prev.education,
-          schoolName: '',
-          schoolAddress: '',
-          schoolLocation: '',
-          schoolCode: '',
-          teacherPhoneNumber: '',
-          teacherName: '',
-          teacherMobilePhoneNumber: '',
-        },
-      }));
-    }
-  }, [form.education.graduationType, setForm]);
 
   const handle출신학교및학력Change: ChangeEventHandler<HTMLInputElement> = (e) => {
     const { name, value } = e.target;
@@ -84,7 +34,10 @@ export const useInput = () => {
       return;
     }
 
-    setForm((prev) => ({ ...prev, education: { ...prev.education, [name]: value } }));
+    setForm((prev) => ({
+      ...prev,
+      education: { ...prev.education, [name]: value },
+    }));
   };
 
   return { handle출신학교및학력Change };
