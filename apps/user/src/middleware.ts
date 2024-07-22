70b28478a4773addf45ed77ffe4dd502046e487b
junghongseop <a01082372487@gmail.com>
2024-07-22 23:42:09 +0900

    refactor(user): 태블릿으로 접속 안되도록 코드 리팩토링

--- apps/user/src/middleware.ts
+++ apps/user/src/middleware.ts
@@ -5,7 +5,7 @@ export const middleware = (request: NextRequest) => {
   const { device } = userAgent(request);
   const url = request.nextUrl.pathname;
 
-  if (device.type === 'mobile' && url !== '/mobile') {
+  if ((device.type === 'mobile' || device.type === 'tablet') && url !== '/mobile') {
     return NextResponse.rewrite(new URL('/mobile', request.url));
   }
 
