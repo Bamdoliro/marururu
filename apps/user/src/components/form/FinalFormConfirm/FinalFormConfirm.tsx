a03e973f81eac4a4fa9eac94e29442ea1505c149
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2024-09-03 16:00:30 +0900

    fix(user): 문구 수정

--- apps/user/src/components/form/FinalFormConfirm/FinalFormConfirm.tsx
+++ apps/user/src/components/form/FinalFormConfirm/FinalFormConfirm.tsx
@@ -14,18 +14,17 @@ const FinalFormConfirm = ({ isOpen, onClose, onConfirm }: Props) => {
       title="최종 제출 하시겠습니까?"
       content={
         <Column>
-          <Text color={color.red} fontType="p2">
+          <Text color={color.gray900} fontType="p2">
             모든 제출서류를 업로드하셨다면 원서를 최종 제출 해주세요.
           </Text>
-          <Text color={color.gray900} fontType="p2">
-            원서 최종 제출 시 입학원서 접수가 완료되며
-            <br /> 더 이상 수정이 불가능합니다.
+          <Text color={color.red} fontType="p2">
+            원서 최종 제출 시 입학원서 접수가 완료되며 더 이상 수정이 불가합니다.
           </Text>
         </Column>
       }
       onClose={onClose}
       onConfirm={onConfirm}
-      confirmButtonText="최종 제출하기"
+      confirmButtonText="원서 최종 제출하기"
     />
   );
 };
