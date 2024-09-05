e28903a65c228c8c7d4191552c2e1fb953e58572
junghongseop <a01082372487@gmail.com>
2024-09-05 10:49:51 +0900

    chore(user): 2분마다 자동 저장되는 코드 삭제

--- apps/user/src/components/common/Wrappers/FormWrapper/FormWrapper.tsx
+++ apps/user/src/components/common/Wrappers/FormWrapper/FormWrapper.tsx
@@ -1,8 +1,5 @@
 'use client';
 
-import { useSaveFormMutation } from '@/services/form/mutations';
-import { useFormValueStore } from '@/store';
-import { useInterval } from '@toss/react';
 import type { ReactNode } from 'react';
 import FormStatusManager from './FormStatusManager/FormStatusManager';
 import SaveFormManager from './SaveFormManager/SaveFormManager';
@@ -12,13 +9,6 @@ interface Props {
 }
 
 const FormWrapper = ({ children }: Props) => {
-  const { saveFormMutate } = useSaveFormMutation();
-  const form = useFormValueStore();
-
-  useInterval(() => {
-    saveFormMutate(form);
-  }, 6000 * 10 * 2);
-
   return (
     <>
       <FormStatusManager />
