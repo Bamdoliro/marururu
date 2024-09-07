38969f97ed1984ef9417e284667ffa51e9c25e1c
junghongseop <a01082372487@gmail.com>
2024-09-08 05:34:05 +0900

    fix(user): 문구 수정

--- apps/user/src/components/main/ApplicationSubmittionBox/ApplicationPeriodBox/ApplicationPeriodBox.tsx
+++ apps/user/src/components/main/ApplicationSubmittionBox/ApplicationPeriodBox/ApplicationPeriodBox.tsx
@@ -10,7 +10,7 @@ const ApplicationPeriodBox = () => {
       <Text fontType="H1" color={color.white}>
         지금은 입학전형 기간이 아닙니다.
         <br />
-        현재 회원가입을 진행할 수 없습니다.
+        현재 회원가입을 진행할 수 없습니다!
       </Text>
       {/* <Text fontType="p2" color={color.gray300}>
         {applicationStart} ~ {applicationEnd}
