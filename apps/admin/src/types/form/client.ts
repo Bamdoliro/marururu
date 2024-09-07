d1c7596c6391f04dcc892e59558eacc399a01963
junghongseop <a01082372487@gmail.com>
2024-09-08 05:24:10 +0900

    chore(admin): eslint에러 해결

--- apps/admin/src/types/form/client.ts
+++ apps/admin/src/types/form/client.ts
@@ -44,7 +44,11 @@ export interface Form {
 
 export type FormListType = '모두 보기' | '검토해야 하는 원서 모아보기' | '정렬';
 
-export type FormListSortingType = 'TOTAL_SCORE_DESC' | 'TOTAL_SCORE_ASC' | 'FORM_ID' | null;
+export type FormListSortingType =
+  | 'TOTAL_SCORE_DESC'
+  | 'TOTAL_SCORE_ASC'
+  | 'FORM_ID'
+  | null;
 
 export type ExportExcelType =
   | '전체 내보내기'
