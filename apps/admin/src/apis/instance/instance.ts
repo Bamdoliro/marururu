14702e79101538febc7c34702af3abb38d00341d
junghongseop <a01082372487@gmail.com>
2024-10-10 01:33:28 +0900

    feat(admin): 토큰 재발급 관련 훅, 코드 수정및 개발

--- apps/admin/src/apis/instance/instance.ts
+++ apps/admin/src/apis/instance/instance.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { refreshToken } from '../token';
 
 export const maru = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
@@ -17,11 +18,22 @@ maru.interceptors.request.use(
   }
 );
 
-maru.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    return Promise.reject(error);
-  }
-);
+export const setupInterceptor = (setAccessToken: (newAccessToken: string) => void) => {
+  maru.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      const originalRequest = error.config;
+      if (error.response.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+        try {
+          const newAccessToken = await refreshToken(setAccessToken);
+          originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+          return maru(originalRequest);
+        } catch (refreshError) {
+          return Promise.reject(refreshError);
+        }
+      }
+      return Promise.reject(error);
+    }
+  );
+};
---
+++ apps/admin/src/hooks/useRefreshToken.ts
@@ -0,0 +1,43 @@
+import { setupInterceptor } from '@/apis/instance/instance';
+import { Session } from '@/apis/session/session';
+import { refreshToken } from '@/apis/token';
+import { ROUTES } from '@/constants/common/constant';
+import { useAccessTokenStore } from '@/store/auth/auth';
+import { useRouter } from 'next/navigation';
+import { useEffect, useRef } from 'react';
+
+const useRefreshToken = () => {
+  const router = useRouter();
+  const [accessToken, setAccessToken] = useAccessTokenStore();
+  const refreshingRef = useRef(false);
+  const token = Session.getRefreshToken();
+
+  useEffect(() => {
+    setupInterceptor(setAccessToken);
+
+    const checkAndRefreshToken = async () => {
+      if (token) {
+        if (!accessToken && !refreshingRef.current) {
+          refreshingRef.current = true;
+          const refreshTokenValue = Session.getRefreshToken();
+          if (refreshTokenValue) {
+            try {
+              await refreshToken(setAccessToken);
+            } catch (error) {
+              router.push(ROUTES.MAIN);
+              sessionStorage.clear();
+            }
+          } else {
+            router.push(ROUTES.MAIN);
+            sessionStorage.clear();
+          }
+          refreshingRef.current = false;
+        }
+      }
+    };
+
+    checkAndRefreshToken();
+  }, [accessToken, setAccessToken, router, token]);
+};
+
+export default useRefreshToken;
