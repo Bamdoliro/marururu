c498ca105332f64ff9818ad0ffe2cf7c1638e113
junghongseop <a01082372487@gmail.com>
2024-10-11 14:57:18 +0900

    fix(user): 원서 시작 시간 수정

--- apps/user/src/components/main/ScheduleBox/ScheduleBox.tsx
+++ apps/user/src/components/main/ScheduleBox/ScheduleBox.tsx
@@ -17,7 +17,7 @@ import ScheduleItem from './ScheduleBoxItem/ScheduleBoxItem';
 const SCHEDULE_DATA = [
   {
     id: 0,
-    date: '2024년 10월 14일 ~ 17일 17:00',
+    date: '2024년 10월 14일 09:00 ~ 17일 17:00',
     plan: '1차 원서 접수',
     startTime: 제출_시작_날짜,
     endTime: 제출_마감_날짜,
