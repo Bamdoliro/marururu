692b141906a7f7d6636441a153d92b16947ac9ec
junghongseop <a01082372487@gmail.com>
2024-10-13 03:12:59 +0900

    feat(admin): 쿠키에 저장하는 클래스 개발

---
+++ apps/admin/src/apis/cookie/cookie.ts
@@ -0,0 +1,21 @@
+import { Cookies } from 'react-cookie';
+
+type CookieKey = 'refresh-token';
+
+const cookies = new Cookies();
+
+export class Cookie {
+  static getItem(key: CookieKey) {
+    return typeof window !== 'undefined' ? cookies.get(key) : null;
+  }
+
+  static setItem(key: CookieKey, value: string) {
+    if (typeof window === 'undefined') return;
+    cookies.set(key, value, { path: '/', httpOnly: true });
+  }
+
+  static removeItem(key: CookieKey) {
+    if (typeof window === 'undefined') return;
+    cookies.remove(key, { path: '/' });
+  }
+}
