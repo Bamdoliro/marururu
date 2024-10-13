6dc003fe4579d40e4c1776fdb184218a56e45f08
junghongseop <a01082372487@gmail.com>
2024-10-14 01:58:10 +0900

    fix(user, admin): 재발급 이슈 수정

--- apps/user/src/apis/token/refresh.ts
+++ apps/user/src/apis/token/refresh.ts
@@ -4,21 +4,18 @@ import { Cookie } from '../cookie/cookie';
 import { Storage } from '../storage/storage';
 
 const refreshToken = async () => {
-  const refreshTokenValue = Cookie.getItem(TOKEN.REFRESH);
-
-  if (!refreshTokenValue) {
-    alert('다시 로그인 해주세요.');
+  try {
+    const { data } = await maru.patch('/auth', null, {
+      headers: {
+        'Refresh-Token': `${Cookie.getItem(TOKEN.REFRESH)}`,
+      },
+    });
+    Storage.setItem(TOKEN.ACCESS, data.data.accessToken);
+  } catch {
     window.location.href = ROUTES.LOGIN;
+    alert('다시 로그인 해주세요');
     localStorage.clear();
-    return;
   }
-
-  const { data } = await maru.patch('/auth', null, {
-    headers: {
-      'Refresh-Token': refreshTokenValue,
-    },
-  });
-  Storage.setItem(TOKEN.ACCESS, data.data.accessToken);
 };
 
 export default refreshToken;
